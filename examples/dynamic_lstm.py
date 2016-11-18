30e04c3d375e91567e26284ca18f740056e1b9ba
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2016-11-17 22:39:57 -0500

    Distributed data parallel LSTM. Use toy data: 50-50 chance of linear and random sequence. Use MPI tensorflow launcher with Slurm

---
+++ examples/dynamic_lstm.py
@@ -0,0 +1,328 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import math
+import sys
+import time
+
+
+import tensorflow as tf
+import random
+import tempfile
+
+NUM_GPUS = 4
+debug = True
+data_dir = '/scratch/network/alexeys'
+from mpi_launch_tensorflow import get_mpi_cluster_server_jobname
+
+flags = tf.app.flags
+#flags.DEFINE_string("data_dir", "/scratch/network/alexeys/",
+#                    "Directory for storing mnist data")
+#flags.DEFINE_boolean("download_only", False,
+#                     "Only perform downloading of data; Do not proceed to "
+#                     "session preparation, model definition or training")
+flags.DEFINE_integer("task_index", None,
+                     "Worker task index, should be >= 0. task_index=0 is "
+                     "the master worker task the performs the variable "
+                     "initialization ")
+flags.DEFINE_integer("replicas_to_aggregate", None,
+                     "Number of replicas to aggregate before parameter update"
+                     "is applied (For sync_replicas mode only; default: "
+                     "num_workers)")
+flags.DEFINE_integer("hidden_units", 100,
+                     "Number of units in the hidden layer of the NN")
+flags.DEFINE_integer("train_steps", 20000,
+                     "Number of (global) training steps to perform")
+flags.DEFINE_integer("batch_size", 100, "Training batch size")
+flags.DEFINE_float("learning_rate", 0.01, "Learning rate")
+flags.DEFINE_boolean("sync_replicas", False,
+                     "Use the sync_replicas (synchronized replicas) mode, "
+                     "wherein the parameter updates from workers are aggregated "
+                     "before applied to avoid stale gradients")
+
+FLAGS = flags.FLAGS
+
+
+# ====================
+#  TOY DATA GENERATOR
+# ====================
+class ToySequenceData(object):
+    """ Generate sequence of data with dynamic length.
+    This class generate samples for training:
+    - Class 0: linear sequences (i.e. [0, 1, 2, 3,...])
+    - Class 1: random sequences (i.e. [1, 3, 10, 7,...])
+
+    NOTICE:
+    We have to pad each sequence to reach 'max_seq_len' for TensorFlow
+    consistency (we cannot feed a numpy array with inconsistent
+    dimensions). The dynamic calculation will then be perform thanks to
+    'seqlen' attribute that records every actual sequence length.
+    """
+    def __init__(self, n_samples=1000, max_seq_len=20, min_seq_len=3,
+                 max_value=1000):
+        self.data = []
+        self.labels = []
+        self.seqlen = []
+        for i in range(n_samples):
+            # Random sequence length
+            len = random.randint(min_seq_len, max_seq_len)
+            # Monitor sequence length for TensorFlow dynamic calculation
+            self.seqlen.append(len)
+            # Add a random or linear int sequence (50% prob)
+            if random.random() < .5:
+                # Generate a linear sequence
+                rand_start = random.randint(0, max_value - len)
+                s = [[float(i)/max_value] for i in
+                     range(rand_start, rand_start + len)]
+                # Pad sequence for dimension consistency
+                s += [[0.] for i in range(max_seq_len - len)]
+                self.data.append(s)
+                self.labels.append([1., 0.])
+            else:
+                # Generate a random sequence
+                s = [[float(random.randint(0, max_value))/max_value]
+                     for i in range(len)]
+                # Pad sequence for dimension consistency
+                s += [[0.] for i in range(max_seq_len - len)]
+                self.data.append(s)
+                self.labels.append([0., 1.])
+        self.batch_id = 0
+
+    def next(self, batch_size):
+        """ Return a batch of data. When dataset end is reached, start over.
+        """
+        if self.batch_id == len(self.data):
+            self.batch_id = 0
+        batch_data = (self.data[self.batch_id:min(self.batch_id +
+                                                  batch_size, len(self.data))])
+        batch_labels = (self.labels[self.batch_id:min(self.batch_id +
+                                                  batch_size, len(self.data))])
+        batch_seqlen = (self.seqlen[self.batch_id:min(self.batch_id +
+                                                  batch_size, len(self.data))])
+        self.batch_id = min(self.batch_id + batch_size, len(self.data))
+        return batch_data, batch_labels, batch_seqlen
+
+def dynamicRNN(x, seqlen, seq_max_len, n_hidden, weights, biases):
+
+    # Prepare data shape to match `rnn` function requirements
+    # Current data input shape: (batch_size, n_steps, n_input)
+    # Required shape: 'n_steps' tensors list of shape (batch_size, n_input)
+
+    # Permuting batch_size and n_steps
+    x = tf.transpose(x, [1, 0, 2])
+    # Reshaping to (n_steps*batch_size, n_input)
+    x = tf.reshape(x, [-1, 1])
+    # Split to get a list of 'n_steps' tensors of shape (batch_size, n_input)
+    x = tf.split(0, seq_max_len, x)
+
+    # Define a lstm cell with tensorflow
+    lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(n_hidden)
+
+    # Get lstm cell output, providing 'sequence_length' will perform dynamic
+    # calculation.
+    outputs, states = tf.nn.rnn(lstm_cell, x, dtype=tf.float32,
+                                sequence_length=seqlen)
+
+    # When performing dynamic calculation, we must retrieve the last
+    # dynamically computed output, i.e., if a sequence length is 10, we need
+    # to retrieve the 10th output.
+    # However TensorFlow doesn't support advanced indexing yet, so we build
+    # a custom op that for each sample in batch size, get its length and
+    # get the corresponding relevant output.
+
+    # 'outputs' is a list of output at every timestep, we pack them in a Tensor
+    # and change back dimension to [batch_size, n_step, n_input]
+    outputs = tf.pack(outputs)
+    outputs = tf.transpose(outputs, [1, 0, 2])
+
+    # Hack to build the indexing and retrieve the right output.
+    batch_size = tf.shape(outputs)[0]
+    # Start indices for each sample
+    index = tf.range(0, batch_size) * seq_max_len + (seqlen - 1)
+    # Indexing
+    outputs = tf.gather(tf.reshape(outputs, [-1, n_hidden]), index)
+
+    # Linear activation, using outputs computed above
+    return tf.matmul(outputs, weights['out']) + biases['out']
+
+
+
+def main(unused_argv):
+
+  cluster,server,job_name,task_index,num_workers = get_mpi_cluster_server_jobname(num_ps = 1, num_workers = 2)
+  if job_name == "ps":
+    server.join()
+
+  is_chief = (task_index == 0)
+  #FIXME num_gpus should be related to NUM_GPUS 
+  if NUM_GPUS > 0:
+    if NUM_GPUS < num_workers:
+      raise ValueError("number of gpus is less than number of workers")
+    # Avoid gpu allocation conflict: now allocate task_num -> #gpu 
+    # for each worker in the corresponding machine
+    MY_GPU = (task_index % NUM_GPUS)
+    worker_device = "/job:worker/task:%d/gpu:%d" % (task_index, MY_GPU)
+  elif NUM_GPUS == 0:
+      raise ValueError("number of gpus is zero")
+  #  # Just allocate the CPU to worker server
+  #  cpu = 0
+  #  worker_device = "/job:worker/task:%d/cpu:%d" % (task_index, cpu)
+  # The device setter will automatically place Variables ops on separate
+  # parameter servers (ps). The non-Variable ops will be placed on the workers.
+  # The ps use CPU and workers use corresponding GPU
+  with tf.device(
+      tf.train.replica_device_setter(
+          worker_device=worker_device,
+          ps_device="/job:ps/cpu:0",
+          cluster=cluster)):
+    global_step = tf.Variable(0, name="global_step", trainable=False)
+
+    # Parameters
+    display_step = 10
+
+    # Network Parameters
+    #The rest of parameters is provided through the FLAGS
+    seq_max_len = 20 # Sequence max length
+    n_classes = 2 # linear sequence or not
+
+    #FIXME this is where data is
+    trainset = ToySequenceData(n_samples=1000, max_seq_len=seq_max_len)
+    testset = ToySequenceData(n_samples=500, max_seq_len=seq_max_len)
+
+    # tf Graph input
+    x = tf.placeholder("float", [None, seq_max_len, 1])
+    y = tf.placeholder("float", [None, n_classes])
+    # A placeholder for indicating each sequence length
+    seqlen = tf.placeholder(tf.int32, [None])
+
+    # Define weights
+    weights = {
+      'out': tf.Variable(tf.random_normal([FLAGS.hidden_units, n_classes]))
+    }
+    biases = {
+      'out': tf.Variable(tf.random_normal([n_classes]))
+    }
+
+    #FIXME to be tested
+    pred = dynamicRNN(x, seqlen, seq_max_len, FLAGS.hidden_units, weights, biases)
+
+    # Define loss and optimizer
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+    opt = tf.train.GradientDescentOptimizer(learning_rate=FLAGS.learning_rate) #.minimize(cost)
+
+    # Evaluate model
+    correct_pred = tf.equal(tf.argmax(pred,1), tf.argmax(y,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+    if FLAGS.sync_replicas:
+      if FLAGS.replicas_to_aggregate is None:
+        replicas_to_aggregate = num_workers
+      else:
+        replicas_to_aggregate = FLAGS.replicas_to_aggregate
+
+      opt = tf.train.SyncReplicasOptimizerV2(
+          opt,
+          replicas_to_aggregate=replicas_to_aggregate,
+          total_num_replicas=num_workers,
+          name="lstm_sync_replicas")
+
+    train_step = opt.minimize(cost, global_step=global_step)
+
+    if FLAGS.sync_replicas:
+      local_init_op = opt.local_step_init_op
+      if is_chief:
+        local_init_op = opt.chief_init_op
+
+      ready_for_local_init_op = opt.ready_for_local_init_op
+
+      # Initial token and chief queue runners required by the sync_replicas mode
+      chief_queue_runner = opt.get_chief_queue_runner()
+      sync_init_op = opt.get_init_tokens_op()
+
+    init_op = tf.initialize_all_variables()
+    train_dir = tempfile.mkdtemp()
+
+    if FLAGS.sync_replicas:
+      sv = tf.train.Supervisor(
+          is_chief=is_chief,
+          logdir=train_dir,
+          init_op=init_op,
+          local_init_op=local_init_op,
+          ready_for_local_init_op=ready_for_local_init_op,
+          recovery_wait_secs=1,
+          global_step=global_step)
+    else:
+      sv = tf.train.Supervisor(
+          is_chief=is_chief,
+          logdir=train_dir,
+          init_op=init_op,
+          recovery_wait_secs=1,
+          global_step=global_step)
+
+    sess_config = tf.ConfigProto(
+        allow_soft_placement=True,
+        log_device_placement=False,
+        device_filters=["/job:ps", "/job:worker/task:%d" % task_index])
+
+    # The chief worker (task_index==0) session will prepare the session,
+    # while the remaining workers will wait for the preparation to complete.
+    if is_chief:
+      print("Worker %d: Initializing session..." % task_index)
+    else:
+      print("Worker %d: Waiting for session to be initialized..." % task_index)
+
+    #if FLAGS.existing_servers:
+    #  server_grpc_url = "grpc://" + worker_spec[task_index]
+    #  print("Using existing server at: %s" % server_grpc_url)
+    #
+    #  sess = sv.prepare_or_wait_for_session(server_grpc_url,
+    #                                        config=sess_config)
+    #else:
+    sess = sv.prepare_or_wait_for_session(server.target, config=sess_config)
+
+    print("Worker %d: Session initialization complete." % task_index)
+
+    if FLAGS.sync_replicas and is_chief:
+      # Chief worker will start the chief queue runner and call the init op.
+      sess.run(sync_init_op)
+      sv.start_queue_runners(sess, [chief_queue_runner])
+
+    # Perform training
+    time_begin = time.time()
+    print("Training begins @ %f" % time_begin)
+
+    local_step = 0
+    while True:
+      # Training feed
+      batch_xs, batch_ys, batch_seqlen = trainset.next(FLAGS.batch_size)
+      train_feed = {x: batch_xs, y: batch_ys, seqlen: batch_seqlen}
+
+      _, step = sess.run([train_step, global_step], feed_dict=train_feed)
+      local_step += 1
+
+      now = time.time()
+      print("%f: Worker %d: training step %d done (global step: %d)" %
+            (now, task_index, local_step, step))
+      if debug:
+          with open("/home/alexeys/bla"+str(task_index)+".txt","w") as fff:
+              fff.write("SUCCESS")
+
+      if step >= FLAGS.train_steps:
+        break
+
+    time_end = time.time()
+    print("Training ends @ %f" % time_end)
+    training_time = time_end - time_begin
+    print("Training elapsed time: %f s" % training_time)
+
+    # Calculate accuracy
+    #test_data = testset.data
+    #test_label = testset.labels
+    #test_seqlen = testset.seqlen
+    #print("Testing Accuracy:", \
+    #    sess.run(accuracy, feed_dict={x: test_data, y: test_label,
+    #                                  seqlen: test_seqlen}))
+
+if __name__ == "__main__":
+  tf.app.run()
