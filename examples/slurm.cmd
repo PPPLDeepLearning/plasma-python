0fcddba8e08b45ca3b83ccfcf1d5bfce55048f6f
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2016-11-22 14:29:56 -0500

    1 MPI process per GPU for data parallel training

--- examples/slurm.cmd
+++ examples/slurm.cmd
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -t 05:00:00
 #SBATCH -N 1
-#SBATCH --ntasks-per-node=16
-#SBATCH --ntasks-per-socket=8
+#SBATCH --ntasks-per-node=4
+#SBATCH --ntasks-per-socket=2
 #SBATCH --gres=gpu:4
 
 module load anaconda
