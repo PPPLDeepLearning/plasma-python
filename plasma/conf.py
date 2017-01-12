e322a26c7cb05e926c28193b2d7b6abd10daef8a
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-01-11 23:52:04 -0500

    Work in progress, useful if conf.yaml is a part of plasma package

--- plasma/conf.py
+++ plasma/conf.py
@@ -2,9 +2,9 @@ from plasma.conf_parser import parameters
 import os
 import errno
 
-try:
+if os.path.exists('./conf.yaml'):
     conf = parameters('./conf.yaml')
-except: 
-    raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), "conf.yaml")
-
-#conf = parameters(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'conf.yaml'))
+elif os.path.exists(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'conf.yaml')):
+    conf = parameters(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'conf.yaml'))
+else:
+    raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), 'conf.yaml')
