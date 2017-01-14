a5734482304bc2b09b263d25525f405b51802952
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-01-14 15:28:38 -0500

    Move signals_dirs to yaml

--- plasma/conf_parser.py
+++ plasma/conf_parser.py
@@ -5,19 +5,7 @@ import yaml
 def parameters(input_file):
     """Parse yaml file of configuration parameters."""
 
-
-    #signal dirs
-    signals_dirs = [['jpf/da/c2-ipla'], # Plasma Current [A]
-                ['jpf/da/c2-loca'], # Mode Lock Amplitude [A]
-                ['jpf/db/b5r-ptot>out'], #Radiated Power [W]
-                ['jpf/gs/bl-li<s'], #Plasma Internal Inductance
-                ['jpf/gs/bl-fdwdt<s'], #Stored Diamagnetic Energy (time derivative) [W] Might cause a lot of false positives!
-                ['jpf/gs/bl-ptot<s'], #total input power [W]
-                ['jpf/gs/bl-wmhd<s']] #total stored diamagnetic energy
-
-    #density signals [m^-2]
-    #4 vertical channels and 4 horizontal channels
-    signals_dirs += [['jpf/df/g1r-lid:{:03d}'.format(i) for i in range(2,9)]]
+    signals_dirs = yaml.load(open(input_file, 'r'))['paths']['signals_dirs']
 
     #signal masks
     signals_masks = [[True]*len(sig_list) for sig_list in signals_dirs]
@@ -30,20 +18,6 @@ def parameters(input_file):
     positivity_mask[0] = [False]
     positivity_mask[4] = [False]
 
-    #radiation signals
-    #vertical signals, don't use signal 16 and 23
-    # signals_dirs += ['jpf/db/b5vr-pbol:{:03d}'.format(i) for i in range(1,28) if (i != 16 and i != 23)]
-    # signals_dirs += ['jpf/db/b5hr-pbol:{:03d}'.format(i) for i in range(1,24)]
-
-    #ece temperature profiles
-    #temperature of channel i vs time
-    # signals_dirs += ['ppf/kk3/te{:02d}'.format(i) for i in range(1,97)]
-    #radial position of channel i mapped onto midplane vs time
-    # signals_dirs += ['ppf/kk3/rc{:02d}'.format(i) for i in range(1,97)]
-
-    ####radial position of channel i vs time
-    ####signal_paths += ['ppf/kk3/ra{:02d}'.format(i) for i in range(1,97)]
-
     with open(input_file, 'r') as yaml_file:
         params = yaml.load(yaml_file)
 
@@ -53,7 +27,6 @@ def parameters(input_file):
 
         params['paths']['base_path'] = base_path
         params['paths']['signal_prepath'] = base_path + '/signal_data/jet/'
-        params['paths']['signals_dirs'] = signals_dirs
         params['paths']['signals_masks'] = signals_masks
         params['paths']['positivity_mask'] = positivity_mask
         params['paths']['shot_list_dir'] = base_path + '/shot_lists/'
