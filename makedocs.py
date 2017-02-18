fa1e3a1cb4ee1e0387213123111150aa7a66daef
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-02-18 15:02:27 -0500

    Add a script to automatically generate Sphinx docs

---
+++ makedocs.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import importlib
+import inspect
+
+modules = [
+    "plasma.primitives.shots",
+    "plasma.models.builder",
+    "plasma.models.loader",
+    "plasma.models.runner",
+    "plasma.models.targets",
+    "plasma.models.mpi_runner",
+    "plasma.preprocessor.normalize",
+    "plasma.preprocessor.preprocess",
+    "plasma.utils.evaluation",
+    "plasma.utils.performance",
+    "plasma.utils.processing"  
+    ]
+
+modules = {name: importlib.import_module(name) for name in modules}
+
+documented = []
+for moduleName, module in modules.items():
+    for objName in dir(module):
+        obj = getattr(module, objName)
+        if not objName.startswith("_") and callable(obj) and obj.__module__ == moduleName:
+            print objName, obj
+            documented.append(moduleName + "." + objName)
+            if inspect.isclass(obj):
+                open("docs/" + moduleName + "." + objName + ".rst", "w").write(''':orphan:
+
+{0}
+{1}
+
+.. autoclass:: {0}
+    :members:
+    :special-members: __init__, __add__
+    :inherited-members:
+    :show-inheritance:
+'''.format(moduleName + "." + objName, "=" * (len(moduleName) + len(objName) + 1)))
+            else:
+                open("docs/" + moduleName + "." + objName + ".rst", "w").write(''':orphan:
+
+{0}
+{1}
+
+.. autofunction:: {0}
+'''.format(moduleName + "." + objName, "=" * (len(moduleName) + len(objName) + 1)))
