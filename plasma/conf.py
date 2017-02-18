b9843ed1463b071a0b4f85d98fcd1dd6eaa2676c
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-02-18 14:59:45 -0500

    Fixed: when importing plasma.conf in REPL, it looks for the conf.yaml in the __file__ by default

--- plasma/conf.py
+++ plasma/conf.py
@@ -4,7 +4,7 @@ import errno
 
 if os.path.exists('./conf.yaml'):
     conf = parameters('./conf.yaml')
-elif os.path.exists(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'conf.yaml')):
-    conf = parameters(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'conf.yaml'))
+elif os.path.exists(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../examples/conf.yaml')):
+    conf = parameters(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../examples/conf.yaml'))
 else:
     raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), 'conf.yaml')
