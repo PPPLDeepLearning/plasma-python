4cc9ff54846c9e9a26d2804181f74abca5655933
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-02-20 22:20:51 -0500

    Change order of import statements

--- plasma/conf.py
+++ plasma/conf.py
@@ -2,10 +2,10 @@ from plasma.conf_parser import parameters
 import os
 import errno
 
-if os.path.exists('./conf.yaml'):
-    conf = parameters('./conf.yaml')
-elif os.path.exists(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../examples/conf.yaml')):
+if os.path.exists(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../examples/conf.yaml')):
     conf = parameters(os.path.join(os.path.abspath(os.path.dirname(__file__)), '../examples/conf.yaml'))
+elif os.path.exists('./conf.yaml'):
+    conf = parameters('./conf.yaml')
 elif os.path.exists('./examples/conf.yaml'):
     conf = parameters('./examples/conf.yaml')
 else:
