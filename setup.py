769c0c13069ede266ba6beb32bd8c4910cd8405e
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-04-08 19:47:57 -0400

    Bump to version 0.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 #import plasma.version
 
 setup(name = "plasma",
-      version = "0.0.3",
+      version = "0.1.0",
       packages = find_packages(),
       #scripts = [""],
       description = "PPPL deep learning package.",
