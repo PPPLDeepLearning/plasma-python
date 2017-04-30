c1c7fa2f6b21af32ed22c438a2604acd85d6167d
Julian Kates-Harbeck <julian@Julians-MacBook-Pro.local>
2017-04-30 16:09:54 -0400

    refactoring signal handling and data downloading

--- data/get_mdsplus_data.py
+++ data/get_mdsplus_data.py
@@ -1,10 +1,18 @@
+from __future__ import print_function
 '''
 http://www.mdsplus.org/index.php?title=Documentation:Tutorial:RemoteAccess&open=76203664636339686324830207&page=Documentation%2FThe+MDSplus+tutorial%2FRemote+data+access+in+MDSplus
 http://piscope.psfc.mit.edu/index.php/MDSplus_%26_python#Simple_example_of_reading_MDSplus_data
 http://www.mdsplus.org/documentation/beginners/expressions.shtml
 http://www.mdsplus.org/index.php?title=Documentation:Tutorial:MdsObjects&open=76203664636339686324830207&page=Documentation%2FThe+MDSplus+tutorial%2FThe+Object+Oriented+interface+of+MDSPlus
 '''
-from __future__ import print_function
+
+'''TODO
+- mapping to flux surfaces: its not always [0,1]!
+- handling of 1D signals during preprocessing & normalization
+- handling of 1D signals for feeding into RNN (convolutional layers)
+- handling of missing data in shots?
+- TEST
+'''
 from MDSplus import *
 #from pylab import *
 import numpy as np
@@ -15,105 +23,16 @@ import Queue
 import os
 import errno
 
-import gadata
+# import gadata
 
 from plasma.primitives.shots import ShotList
 
+from signal_names import *
+
 #print("Importing numpy version"+np.__version__)
 
-prepath = '/cscratch/share/frnn/'
-shot_numbers_path = 'shot_lists/'
-save_path = 'signal_data/'
-machine = 'd3d'
-
-
-if machine == 'nstx':
-	shot_numbers_files = ['disrupt_nstx.txt'] 
-	server_path = "skylark.pppl.gov:8501::"
-	signal_paths = ['engineering/ip1/',
-	'operations/rwmef_plas_n1_amp_br/',
-	'efit02/li/',
-	'activespec/ts_ld/',
-	'passivespec/bolom_totpwr/',
-	'nbi/nb_p_inj/',
-	'efit02/wpdot/']
-
-elif machine == 'd3d':
-	shot_numbers_files = ['shotlist_JaysonBarr_clear.txt']
-	shot_numbers_files += ['shotlist_JaysonBarr_disrupt.txt']
-	#shot_numbers_files = ['d3d_short_clear.txt']# ,'d3d_clear.txt', 'd3d_disrupt.txt']
-	server_path = 'atlas.gat.com'
-	from d3d_signals import signal_paths	
-	import itertools
-	signal_paths = list(itertools.chain.from_iterable(signal_paths))
-#	signal_paths = ['PINJ','IP','Q95','DENSITY','WMHD'] #,'PRAD'] #PRAD returns a 2D xdata
-#       Recommended signals from DIII-D
-	# signal_paths = ['efsli','ipsip','efsbetan','efswmhd','nssampn1l','nssfrqn1l',
-			# 'nssampn2l','nssfrqn2l',
-			# 'dusbradial','dssdenest',r'\bol_l15_p',r'\bol_l03_p','bmspinj','bmstinj','pcechpwrf']
-	# signal_paths = ['d3d/' + path for path in signal_paths]
-	
-
-
-elif machine == 'jet':
-	shot_numbers_files = ['CWall_clear.txt','CFC_unint.txt','BeWall_clear.txt','ILW_unint.txt']
-	server_path = 'mdsplus.jet.efda.org'
-
-	#plasma current, locked mode, output power
-	signal_paths = ['jpf/da/c2-ipla',
-	'jpf/da/c2-loca',
-	'jpf/db/b5r-ptot>out']
-
-
-
-	#internal inductance, time derivative of stored energy, input power, total diamagnetic energy
-	signal_paths += ['jpf/gs/bl-li<s',
-	'jpf/gs/bl-fdwdt<s',
-	'jpf/gs/bl-ptot<s',
-	'jpf/gs/bl-wmhd<s']
-
-
-
-	
-	#density signals
-	#4 vertical channels and 4 horizontal channels
-	signal_paths += ['jpf/df/g1r-lid:{:03d}'.format(i) for i in range(1,9)]
-
-
-
-	#radiation signals
-	#vertical signals, don't use signal 16 and 23
-	signal_paths += ['jpf/db/b5vr-pbol:{:03d}'.format(i) for i in range(1,28) if (i != 16 and i != 23)]
-	signal_paths += ['jpf/db/b5hr-pbol:{:03d}'.format(i) for i in range(1,24)]
-
-
-	#ece temperature profiles
-	#temperature of channel i vs time
-	signal_paths += ['ppf/kk3/te{:02d}'.format(i) for i in range(1,97)]
-
-	#radial position of channel i vs time
-	#signal_paths += ['ppf/kk3/ra{:02d}'.format(i) for i in range(1,97)]
-
-	#radial position of channel i mapped onto midplane vs time
-	signal_paths += ['ppf/kk3/rc{:02d}'.format(i) for i in range(1,97)]
-
-
-
-
-
-
-
-else:
-	print('unkown machine. exiting')
-	exit(1)
-
-
-
-
-
-
 def create_missing_value_filler():
-	time = np.linspace(0,100,1000)
+	time = np.linspace(0,100,100)
 	vals = np.zeros_like(time)
 	return time,vals
 
@@ -122,27 +41,11 @@ def mkdirdepth(filename):
 	if not os.path.exists(folder):
 		os.makedirs(folder)
 
-
-def get_tree_and_tag(path):
-	spl = path.split('/')
-	tree = spl[0]
-	tag = '\\' + spl[1]
-	return tree,tag
-
-def get_tree_and_tag_no_backslash(path):
-	spl = path.split('/')
-	tree = spl[0]
-	tag = spl[1]
-	return tree,tag
-
-
-
-
 def format_save_path(prepath,signal_path,shot_num):
 	return prepath + signal_path  + '/{}.txt'.format(shot_num)
 
 
-def save_shot(shot_num_queue,c,signal_paths,save_prepath,machine):
+def save_shot_signals(shot_num_queue,complete_queue,c,signals,save_prepath,machine):
 	missing_values = 0
 	# if machine == 'd3d':
 	# 	reload(gadata) #reloads Gadata object with connection
@@ -151,38 +54,21 @@ def save_shot(shot_num_queue,c,signal_paths,save_prepath,machine):
 			shot_num = shot_num_queue.get(False)
 		except Queue.Empty:
 			break
-		for signal_path in signal_paths:
+		shot_complete = True
+		for signal in signals:
+			signal_path = signal.get_path(machine)
 			save_path_full = format_save_path(save_prepath,signal_path,shot_num)
+			success = False
 			if os.path.isfile(save_path_full):
 				print('-',end='')
 			else:
-			    try:
-					if machine == 'nstx':
-						tree,tag = get_tree_and_tag(signal_path)
-						c.openTree(tree,shot_num)
-						data = c.get(tag).data()
-						time = c.get('dim_of('+tag+')').data()
-					elif machine == 'jet':
-						try:
-							data = c.get('_sig=jet("{}/",{})'.format(signal_path,shot_num)).data()
-							time = c.get('_sig=dim_of(jet("{}/",{}))'.format(signal_path,shot_num)).data()
-						except:
-							missing_values += 1
-							print('Signal {}, shot {} missing. Filling with zeros'.format(signal_path,shot_num))
-							time,data = create_missing_value_filler()
-					elif machine == 'd3d':
-						tree,tag = get_tree_and_tag_no_backslash(signal_path)
-						try:
-							ga1 = gadata.gadata('{}'.format(tag),shot_num,tree=tree,connection=c)
-							if not ga1.found:
-								raise
-#							ga1 = gadata.gadata('\\{}'.format(signal_path),shot_num,tree='d3d',connection=c)
-							data = ga1.zdata
-							time = ga1.xdata					
-						except:
-							missing_values += 1
-							print('Signal {}, shot {} missing. Filling with zeros'.format(signal_path,shot_num))
-							time,data = create_missing_value_filler()
+				try:
+					try:
+						time,data,mapping,success = machine.fetch_data(signal,shot_num,c) 
+					except:
+						missing_values += 1
+						print('Signal {}, shot {} missing. Filling with zeros'.format(signal_path,shot_num))
+						time,data = create_missing_value_filler()
 
 					data_two_column = np.vstack((np.atleast_2d(time),np.atleast_2d(data))).transpose()
 					try: #can lead to race condition
@@ -196,14 +82,29 @@ def save_shot(shot_num_queue,c,signal_paths,save_prepath,machine):
 					        raise
 					np.savetxt(save_path_full,data_two_column,fmt = '%.5e')#fmt = '%f %f')
 					print('.',end='')
-			    except:
+				except:
 					print('Could not save shot {}, signal {}'.format(shot_num,signal_path))
 					print('Warning: Incomplete!!!')
 					raise
 			sys.stdout.flush()
-		print('saved shot {}'.format(shot_num))
+			if not success:
+				missing_values += 1
+				shot_complete = False
+		#only add shot to list if it was complete
+		if shot_complete:
+			print('saved shot {}'.format(shot_num))
+			complete_queue.put(shot_num)
+		else:
+			print('shot {} not complete. removing from list.'.format(shot_num))
 	print('Finished with {} missing values total'.format(missing_values))
 
+
+
+prepath = '/cscratch/share/frnn/'
+shot_numbers_path = 'shot_lists/'
+save_path = 'signal_data/'
+machine = d3d
+signals = all_signals
 
 
 
@@ -213,9 +114,10 @@ save_prepath = prepath+save_path + '/' + machine + '/'
 shot_numbers,_ = ShotList.get_multiple_shots_and_disruption_times(prepath + shot_numbers_path,shot_numbers_files)
 
 
-fn = partial(save_shot,signal_paths=signal_paths,save_prepath=save_prepath,machine=machine)
+fn = partial(save_shot,signals=signals,save_prepath=save_prepath,machine=machine)
 num_cores = min(mp.cpu_count(),32) #can only handle 8 connections at once :(
 queue = mp.Queue()
+complete_queue = mp.Queue()
 for shot_num in shot_numbers:
 	queue.put(shot_num)
 connections = [Connection(server_path) for _ in range(num_cores)]
@@ -229,7 +131,9 @@ for p in processes:
 for p in processes:
 	p.join()
 
-print('Finished downloading {} shots in {} seconds'.format(len(shot_numbers),time.time()-start_time))
+complete_shot_numbers = list(complete_queue)
+print('Finished downloading {} ({} complete) shots in {} seconds'.format(len(shot_numbers),len(complete_shot_numbers),time.time()-start_time))
+
 
 # c = Connection(server_path)
 
@@ -244,5 +148,149 @@ print('Finished downloading {} shots in {} seconds'.format(len(shot_numbers),tim
 
 
 
+
+
+# def save_shot(shot_num_queue,c,signal_paths,save_prepath,machine):
+# 	missing_values = 0
+# 	# if machine == 'd3d':
+# 	# 	reload(gadata) #reloads Gadata object with connection
+# 	while True:
+# 		try:
+# 			shot_num = shot_num_queue.get(False)
+# 		except Queue.Empty:
+# 			break
+# 		for signal_path in signal_paths:
+# 			save_path_full = format_save_path(save_prepath,signal_path,shot_num)
+# 			if os.path.isfile(save_path_full):
+# 				print('-',end='')
+# 			else:
+# 			    try:
+# 					if machine == 'nstx':
+# 						tree,tag = get_tree_and_tag(signal_path)
+# 						c.openTree(tree,shot_num)
+# 						data = c.get(tag).data()
+# 						time = c.get('dim_of('+tag+')').data()
+# 					elif machine == 'jet':
+# 						try:
+# 							data = c.get('_sig=jet("{}/",{})'.format(signal_path,shot_num)).data()
+# 							time = c.get('_sig=dim_of(jet("{}/",{}))'.format(signal_path,shot_num)).data()
+# 						except:
+# 							missing_values += 1
+# 							print('Signal {}, shot {} missing. Filling with zeros'.format(signal_path,shot_num))
+# 							time,data = create_missing_value_filler()
+# 					elif machine == 'd3d':
+# 						tree,tag = get_tree_and_tag_no_backslash(signal_path)
+# 						try:
+# 							ga1 = gadata.gadata('{}'.format(tag),shot_num,tree=tree,connection=c)
+# 							if not ga1.found:
+# 								raise
+# #							ga1 = gadata.gadata('\\{}'.format(signal_path),shot_num,tree='d3d',connection=c)
+# 							data = ga1.zdata
+# 							time = ga1.xdata					
+# 						except:
+# 							missing_values += 1
+# 							print('Signal {}, shot {} missing. Filling with zeros'.format(signal_path,shot_num))
+# 							time,data = create_missing_value_filler()
+
+# 					data_two_column = np.vstack((np.atleast_2d(time),np.atleast_2d(data))).transpose()
+# 					try: #can lead to race condition
+# 						mkdirdepth(save_path_full)
+# 					except OSError, e:
+# 					    if e.errno == errno.EEXIST:
+# 					        # File exists, and it's a directory, another process beat us to creating this dir, that's OK.
+# 					        pass
+# 					    else:
+# 					        # Our target dir exists as a file, or different error, reraise the error!
+# 					        raise
+# 					np.savetxt(save_path_full,data_two_column,fmt = '%.5e')#fmt = '%f %f')
+# 					print('.',end='')
+# 			    except:
+# 					print('Could not save shot {}, signal {}'.format(shot_num,signal_path))
+# 					print('Warning: Incomplete!!!')
+# 					raise
+# 			sys.stdout.flush()
+# 		print('saved shot {}'.format(shot_num))
+# 	print('Finished with {} missing values total'.format(missing_values))
+
+
+# if machine == 'nstx':
+# 	shot_numbers_files = ['disrupt_nstx.txt'] 
+# 	server_path = "skylark.pppl.gov:8501::"
+# 	signal_paths = ['engineering/ip1/',
+# 	'operations/rwmef_plas_n1_amp_br/',
+# 	'efit02/li/',
+# 	'activespec/ts_ld/',
+# 	'passivespec/bolom_totpwr/',
+# 	'nbi/nb_p_inj/',
+# 	'efit02/wpdot/']
+
+# elif machine == 'd3d':
+# 	shot_numbers_files = ['shotlist_JaysonBarr_clear.txt']
+# 	shot_numbers_files += ['shotlist_JaysonBarr_disrupt.txt']
+# 	#shot_numbers_files = ['d3d_short_clear.txt']# ,'d3d_clear.txt', 'd3d_disrupt.txt']
+# 	server_path = 'atlas.gat.com'
+# 	from d3d_signals import signal_paths	
+# 	import itertools
+# 	signal_paths = list(itertools.chain.from_iterable(signal_paths))
+# #	signal_paths = ['PINJ','IP','Q95','DENSITY','WMHD'] #,'PRAD'] #PRAD returns a 2D xdata
+# #       Recommended signals from DIII-D
+# 	# signal_paths = ['efsli','ipsip','efsbetan','efswmhd','nssampn1l','nssfrqn1l',
+# 			# 'nssampn2l','nssfrqn2l',
+# 			# 'dusbradial','dssdenest',r'\bol_l15_p',r'\bol_l03_p','bmspinj','bmstinj','pcechpwrf']
+# 	# signal_paths = ['d3d/' + path for path in signal_paths]
+	
+
+
+# elif machine == 'jet':
+# 	shot_numbers_files = ['CWall_clear.txt','CFC_unint.txt','BeWall_clear.txt','ILW_unint.txt']
+# 	server_path = 'mdsplus.jet.efda.org'
+
+# 	#plasma current, locked mode, output power
+# 	signal_paths = ['jpf/da/c2-ipla',
+# 	'jpf/da/c2-loca',
+# 	'jpf/db/b5r-ptot>out']
+
+
+
+# 	#internal inductance, time derivative of stored energy, input power, total diamagnetic energy
+# 	signal_paths += ['jpf/gs/bl-li<s',
+# 	'jpf/gs/bl-fdwdt<s',
+# 	'jpf/gs/bl-ptot<s',
+# 	'jpf/gs/bl-wmhd<s']
+
+
+
+	
+# 	#density signals
+# 	#4 vertical channels and 4 horizontal channels
+# 	signal_paths += ['jpf/df/g1r-lid:{:03d}'.format(i) for i in range(1,9)]
+
+
+
+# 	#radiation signals
+# 	#vertical signals, don't use signal 16 and 23
+# 	signal_paths += ['jpf/db/b5vr-pbol:{:03d}'.format(i) for i in range(1,28) if (i != 16 and i != 23)]
+# 	signal_paths += ['jpf/db/b5hr-pbol:{:03d}'.format(i) for i in range(1,24)]
+
+
+# 	#ece temperature profiles
+# 	#temperature of channel i vs time
+# 	signal_paths += ['ppf/kk3/te{:02d}'.format(i) for i in range(1,97)]
+
+# 	#radial position of channel i vs time
+# 	#signal_paths += ['ppf/kk3/ra{:02d}'.format(i) for i in range(1,97)]
+
+# 	#radial position of channel i mapped onto midplane vs time
+# 	signal_paths += ['ppf/kk3/rc{:02d}'.format(i) for i in range(1,97)]
+
+
+
+
+
+
+
+# else:
+# 	print('unkown machine. exiting')
+# 	exit(1)
 
 
---
+++ data/signals.py
@@ -0,0 +1,221 @@
+from MDSplus import *
+import numpy
+import time
+import sys
+
+# class SignalCollection:
+# 	"""GA Data Obj"""
+# 	def __init__(self,signal_descriptions,signal_paths):
+# 		self.signals = []
+# 		for i in range(len(signal_paths))
+# 			self.signals.append(Signal(signal_descriptions[i],signal_paths[i]))
+
+class Signal:
+	def __init__(self,description,paths,machines=['jet'],tex_label=None,causal_shifts=None)
+		assert(len(paths) == len(machines))
+		self.description = description
+		self.paths = paths
+		self.machines = machines #on which machines is the signal defined
+		if causal_shifts = None:
+			causal_shifts = [0 for m in machines]
+		self.causal_shifts = causal_shifts #causal shift in ms
+
+	def is_defined_on_machine(self,machine):
+		return machine in self.machines
+
+	def is_defined_on_machines(self,machines):
+		return all([m in self.machines for m in machines])
+
+	def get_path(self,machine):
+		idx = self.get_idx(machine)
+		return self.paths[idx]
+
+	def get_causal_shift(self,machine):
+		idx = self.get_idx(machine)
+		return self.causal_shifts[idx]
+
+	def get_idx(self,machine):
+		assert(machine in self.machines)
+		idx = self.machines.index(machine)	
+		return idx
+
+
+class Machine:
+	def __init__(self,name,server,fetch_data_fn):
+		self.name = name
+		self.server = server
+		self.fetch_data_fn = fetch_data_fn
+
+	def get_connection(self):
+		return Connection(server)
+
+	def fetch_data(self,signal,shot_num,c):
+		path = signal.get_path(self)
+		time,data,mapping,success = self.fetch_data_fn(path,shot_num,c)
+		time = time + signal.get_causal_shift(self)
+		return time,data,mapping,success
+
+	def __eq__(self,other):
+		return __eq__(self.name,other.name)
+
+
+
+
+def get_tree_and_tag(path):
+	spl = path.split('/')
+	tree = spl[0]
+	tag = '\\' + spl[1]
+	return tree,tag
+
+def get_tree_and_tag_no_backslash(path):
+	spl = path.split('/')
+	tree = spl[0]
+	tag = spl[1]
+	return tree,tag
+
+def fetch_d3d_data(signal_path,shot_num,c):
+	success = False
+	try:
+		time,data,success = attempt_fetch_d3d_data(signal_path,shot_num,c)
+	except:
+		time,data = create_missing_value_filler()
+	return time,data,success
+
+
+def attempt_fetch_d3d_data(signal_path,shot,c=None):
+	tree,signal = get_tree_and_tag_no_backslash(signal_path)
+	if tree == None:
+		signal = c.get('findsig("'+signal+'",_fstree)').value
+		tree = c.get('_fstree').value 
+	if c is None:
+		c = MDSplus.Connection('atlas.gat.com')
+
+	# ## Retrieve Data 
+	t0 =  time.time()
+	found = False
+	xdata = [0]
+	ydata = None
+	data = [0]
+
+	# Retrieve data from MDSplus (thin)
+	#first try, retrieve directly from tree andsignal 
+	def get_units(str):
+		units = c.get('units_of('+str+')').data()
+		if units == '' or units == ' ': 
+			units = c.get('units('+str+')').data()
+		return units
+
+	try:     
+		c.openTree(fstree,shot)
+		data  = c.get('_s = '+tag).data()
+		data_units = c.get('units_of(_s)').data()  
+		rank = numpy.ndim(data)	
+		if rank > 1:
+			xdata = c.get('dim_of(_s,1)').data()
+			xunits = get_units('dim_of(_s,1)')
+			ydata 	= c.get('dim_of(_s)').data()
+	    	yunits = get_units('dim_of(_s)')
+		else:
+			xdata = c.get('dim_of(_s)').data()
+	    	xunits = get_units('dim_of(_s)')
+		found = True
+		# MDSplus seems to return 2-D arrays transposed.  Change them back.
+		if numpy.ndim(data) == 2: data = numpy.transpose(data)
+		if numpy.ndim(ydata) == 2: ydata = numpy.transpose(ydata)
+		if numpy.ndim(xdata) == 2: xdata = numpy.transpose(xdata)
+
+	except Exception,e:
+		pass
+	# Retrieve data from PTDATA if node not found
+	if not found:
+		data = c.get('_s = ptdata2("'+signal+'",'+str(shot)+')')
+		if len(data) != 1:
+			xdata = c.get('dim_of(_s)')
+			rank = 1
+			found = True
+	# Retrieve data from Pseudo-pointname if not in ptdata
+	if not found:
+		data = c.get('_s = pseudo("'+signal+'",'+str(shot)+')')
+		if len(data) != 1:
+			xdata = c.get('dim_of(_s)')
+			rank = 1
+			found = True
+	#this means the signal wasn't found
+	if not found:  
+		pass
+		#print "   No such signal: %s" % (signal,)
+
+    # print '   GADATA Retrieval Time : ',time.time() - t0
+	return xdata,data,ydata,found
+
+
+def fetch_jet_data(signal_path,shot_num,c):
+	data = c.get('_sig=jet("{}/",{})'.format(signal_path,shot_num)).data()
+	time = c.get('_sig=dim_of(jet("{}/",{}))'.format(signal_path,shot_num)).data()
+	found = True
+	return time,data,None,found
+
+def fetch_nstx_data(signal_path,shot_num,c):
+	tree,tag = get_tree_and_tag(signal_path)
+	c.openTree(tree,shot_num)
+	data = c.get(tag).data()
+	time = c.get('dim_of('+tag+')').data()
+	found = True
+	return time,data,None,found
+
+d3d = Machine("d3d","atlas.gat.com",fetch_d3d_data)
+jet = Machine("jet","mdsplus.jet.efda.org",fetch_jet_data)
+nstk = Machine("nstx","skylark.pppl.gov:8501::",fetch_nstx_data)
+
+
+all_machines = [d3d,jet]
+
+etemp_profile = Signal("Electron temperature profile",["ZIPFIT01/PROFILES.ETEMPPROFILE"],[d3d],causal_shifts=[10])
+edens_profile = Signal("Electron density profile",["ZIPFIT01/PROFILES.EDENSPROFILE"],[d3d],causal_shifts=[10])
+
+q95 = Signal("q95 safety factor",["EFIT02/RESULTS.AEQDSK.Q95"],[d3d],causal_shifts=[10])
+
+li = Signal("locked mode amplitude",["jpf/gs/bl-li<s","d3d/efsli"],[jet,d3d])
+ip = Signal("plasma current",["jpf/da/c2-ipla","d3d/ipsip"])
+betan = Signal("Normalized Beta",['d3d/efsbetan'],[d3d])
+energy = Signal("stored energy",['d3d/efswmhd'],[d3d])
+lm = Signal("Locked mode amplitude",['jpf/da/c2-loca','d3d/dusbradial'],[jet,d3d])
+dens = Signal("Plasma density",['jpf/df/g1r-lid:002','d3d/dssdenest'],[jet,d3d])
+
+pradcore = Signal("Radiated Power Core",['d3d/'+r'\bol_l15_p'],[d3d])
+pradedge = Signal("Radiated Power Edge",['d3d/'+r'\bol_l03_p'],[d3d])
+pradtot = Signal("Radiated Power",['jpf/db/b5r-ptot>out'],[jet])
+pin = Signal("Input Power (beam for d3d)",['jpf/gs/bl-ptot<s','d3d/bmspinj'],[jet,d3d]) #Total Beam Power
+pechin = Signal("ECH input power, not always on",['d3d/pcechpwrf'],[d3d])
+
+torquein = Signal("Input Beam Torque",['d3d/bmstinj'],[d3d]) #Total Beam Power
+tmamp1 = Signal("Tearing Mode amplitude (rotating 2/1)", ['d3d/nssampn1l'],[d3d])
+tmamp2 = Signal("Tearing Mode amplitude (rotating 3/2)", ['d3d/nssampn2l'],[d3d])
+tmfreq1 = Signal("Tearing Mode frequency (rotating 2/1)", ['d3d/nssfrqn1l'],[d3d])
+tmfreq2 = Signal("Tearing Mode frequency (rotating 3/2)", ['d3d/nssfrqn2l'],[d3d])
+
+
+all_signals = [etemp_profile,edens_profile,q95,li,ip,
+betan,energy,lm,dens,pradcore,pradedge,pradtot,pin,
+pechin,torquein,tmamp1,tmamp2,tmfreq1,tmfreq2]
+
+
+fully_defined_signals = [sig for sig in all_signals if sig.is_defined_on_machines(all_machines)]
+
+
+
+#['pcechpwrf'] #Total ECH Power Not always on!
+### 0D EFIT signals ###
+#signal_paths += ['EFIT02/RESULTS.AEQDSK.Q95']
+  
+### 1D EFIT signals ###
+#the other signals give more reliable data
+#signal_paths += [
+#'AOT/EQU.t_e', #electron temperature profile vs rho (uniform mapping over time)
+#'AOT/EQU.dens_e'] #electron density profile vs rho (uniform mapping over time)
+
+
+
+
+
+
