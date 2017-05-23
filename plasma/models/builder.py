84db7d37b450d7f44d4d7ea5768fc6d5e6cf27d2
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-05-23 13:16:14 -0400

    Cleanup

--- plasma/models/builder.py
+++ plasma/models/builder.py
@@ -144,12 +144,6 @@ class ModelBuilder(object):
             return tuple(shape_curr)
 
 	pre_rnn_input = Input(shape=(num_signals,))
-	#print(batch_shape_non_temporal)
-	#print(batch_input_shape)
-	#print(indices_0d)
-	#print(indices_1d)
-	#print(num_0D)
-	#print(num_1D)
 	
 	if num_1D > 0:
             #pre_rnn_0D = Lambda(lambda x: slicer(x,indices_0d),lambda s: slicer_output_shape(s,indices_0d))(pre_rnn_input)
@@ -215,8 +209,6 @@ class ModelBuilder(object):
     			K.get_session().run(tf.global_variables_initializer())
 
         model.reset_states()
-	#model.summary()
-        #model.compile(loss='mean_squared_error', optimizer='sgd') #for numerical output
         return model
 
     def build_train_test_models(self):
