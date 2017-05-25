a7b06989291b5cc4425521b5ee680c8bed34e517
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-05-25 16:01:05 -0400

    Custom float16 MPI datatype and reduction operation

---
+++ plasma/primitives/ops.py
@@ -0,0 +1,15 @@
+from mpi4py import MPI
+comm = MPI.COMM_WORLD
+
+#define a float16 mpi datatype
+mpi_float16 = MPI.BYTE.Create_contiguous(2).Commit()
+MPI._typedict['e'] = mpi_float16
+
+def sum_f16_cb(buffer_a, buffer_b, t):
+    assert t == mpi_float16
+    array_a = np.frombuffer(buffer_a, dtype='float16')
+    array_b = np.frombuffer(buffer_b, dtype='float16')
+    array_b += array_a
+
+#create new OP
+mpi_sum_f16 = MPI.Op.Create(sum_f16_cb, commute=True)
