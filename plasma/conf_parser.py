9ec4d5771dc51952e6c4fe2f55030aa742955181
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-05-25 15:56:22 -0400

    Parse parameters inside conf_parser.py, only import in conf.py

--- plasma/conf_parser.py
+++ plasma/conf_parser.py
@@ -1,4 +1,5 @@
 import plasma.models.targets as t
+from plasma.primitives.shots import ShotListFiles
 import getpass
 import yaml
 
@@ -49,5 +50,29 @@ def parameters(input_file):
         #params['model']['output_activation'] = params['data']['target'].activation
         #binary crossentropy performs slightly better?
         #params['model']['loss'] = params['data']['target'].loss
+
+        from data.signals import *
+        #signals
+        params['paths']['all_signals'] = all_signals
+        #make sure all 1D signals appear last!
+        params['paths']['use_signals'] = d3d_signals
+
+        #shot lists
+        jet_carbon_wall = ShotListFiles(jet,params['paths']['shot_list_dir'],['CWall_clear.txt','CFC_unint.txt'],'jet carbon wall data')
+        jet_iterlike_wall = ShotListFiles(jet,params['paths']['shot_list_dir'],['ILW_unint.txt','BeWall_clear.txt'],'jet iter like wall data')
+        jet_full = ShotListFiles(jet,params['paths']['shot_list_dir'],['ILW_unint.txt','BeWall_clear.txt','CWall_clear.txt','CFC_unint.txt'],'jet full data')
+
+        d3d_10000 = ShotListFiles(d3d,params['paths']['shot_list_dir'],['d3d_clear_10000.txt','d3d_disrupt_10000.txt'],'d3d data 10000 ND and D shots')
+        d3d_1000 = ShotListFiles(d3d,params['paths']['shot_list_dir'],['d3d_clear_1000.txt','d3d_disrupt_1000.txt'],'d3d data 1000 ND and D shots')
+        d3d_100 = ShotListFiles(d3d,params['paths']['shot_list_dir'],['d3d_clear_100.txt','d3d_disrupt_100.txt'],'d3d data 100 ND and D shots')
+        d3d_full = ShotListFiles(d3d,params['paths']['shot_list_dir'],['d3d_clear_data_avail.txt','d3d_disrupt_data_avail.txt'],'d3d data since shot 125500')
+        d3d_jb_full = ShotListFiles(d3d,params['paths']['shot_list_dir'],['shotlist_JaysonBarr_clear.txt','shotlist_JaysonBarr_disrupt.txt'],'d3d shots since 160000-170000')
+
+        nstx_full = ShotListFiles(nstx,params['paths']['shot_list_dir'],['disrupt_nstx.txt'],'nstx shots (all are disruptive')
+
+        params['paths']['shot_files'] = [d3d_full]
+        params['paths']['shot_files_test'] = []
+        params['paths']['shot_files_all'] = params['paths']['shot_files']+params['paths']['shot_files_test']
+        params['paths']['all_machines'] = list(set([file.machine for file in params['paths']['shot_files_all']]))
 
     return params
