2f547361fd8d3f45cf9e7cfcfe375eb7e6ceaceb
Julian Kates-Harbeck <jk7@tigergpu.princeton.edu>
2017-05-28 19:22:32 -0400

    added numerical stability to numpy based binary crossentropy

--- plasma/utils/evaluation.py
+++ plasma/utils/evaluation.py
@@ -1,4 +1,5 @@
 import numpy as np
+epsilon = 1e-7
 
 def get_loss_from_list(y_pred_list,y_true_list,target):
     return np.mean([get_loss(yg,yp,target) for yp,yg in zip(y_pred_list,y_true_list)])
@@ -13,6 +14,7 @@ def mse_np(y_true,y_pred):
     return np.mean((y_pred-y_true)**2)
 
 def binary_crossentropy_np(y_true,y_pred):
+    y_pred = np.clip(y_pred,epsilon,1-epsilon)
     return np.mean(- (y_true*np.log(y_pred) + (1-y_true)*np.log(1 - y_pred)))
 
 def hinge_np(y_true,y_pred):
