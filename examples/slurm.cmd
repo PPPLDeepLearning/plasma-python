ca4e2c86dc9abd5f0f9e3d8d6a425059b4eb2c25
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-06-24 16:51:38 -0400

    Update Slurm submission file to CUDA8, later version of MPI

--- examples/slurm.cmd
+++ examples/slurm.cmd
@@ -1,12 +1,18 @@
 #!/bin/bash
-#SBATCH -t 01:00:00
-#SBATCH -N 1
+#SBATCH -t 01:30:00
+#SBATCH -N 3
 #SBATCH --ntasks-per-node=4
 #SBATCH --ntasks-per-socket=2
 #SBATCH --gres=gpu:4
+#SBATCH -c 4
 
 
 module load anaconda
 source activate PPPL
-module load cudatoolkit/7.5 cudann openmpi/intel-16.0/1.8.8/64
+module load cudatoolkit/8.0 cudann/cuda-8.0/5.1 openmpi/intel-17.0/1.10.2/64 intel/17.0/64/17.0.2.174
+
+rm /tigress/alexeys/model_checkpoints/*
+rm /tigress/alexeys/csv_logs/*
+rm /tigress/alexeys/Graph/*
+
 srun python mpi_learn.py
