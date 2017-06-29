37ec81993930f50c0e6d6b329d41923aaa9505cc
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-06-29 16:03:53 -0400

    Actual test

--- examples/jenkins.sh
+++ examples/jenkins.sh
@@ -15,4 +15,4 @@ module load cudatoolkit/8.0 cudann/cuda-8.0/5.1
 module load openmpi/intel-17.0/2.1.0/64 intel/17.0/64/17.0.4.196 intel-mkl/2017.3/4/64
 
 echo $SLURM_NODELIST
-#mpirun -npernode 4 python mpi_learn.py
+mpirun -npernode 4 python mpi_learn.py
