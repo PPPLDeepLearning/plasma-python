619bb71615660bc2c05eb78da34ae581a5042351
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-08-10 10:43:53 -0400

    Tensorflow1.3 requires CuDNN6+

--- examples/jenkins.sh
+++ examples/jenkins.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-rm /tigress/alexeys/results/*
 rm /tigress/alexeys/model_checkpoints/*
-rm /tigress/alexeys/csv_logs/*
-rm /tigress/alexeys/Graph/*
 
 ls ${PWD}
 
 module load anaconda
-source activate PPPL
-module load cudatoolkit/8.0 cudann/cuda-8.0/5.1
+module load cudatoolkit/8.0
 module load openmpi/intel-17.0/2.1.0/64 intel/17.0/64/17.0.4.196 intel-mkl/2017.3/4/64
+module load cudnn/cuda-8.0/6.0
+source activate PPPL
+
+export OMPI_MCA_btl="tcp,self,sm"
 
 echo $SLURM_NODELIST
 srun python mpi_learn.py
--- examples/slurm.cmd
+++ examples/slurm.cmd
@@ -8,11 +8,18 @@
 
 
 module load anaconda
+module load cudatoolkit/8.0
+module load openmpi/intel-17.0/2.1.0/64 intel/17.0/64/17.0.4.196 intel-mkl/2017.3/4/64
+module load cudnn/cuda-8.0/6.0
 source activate PPPL
-module load cudatoolkit/8.0 cudann/cuda-8.0/5.1 openmpi/intel-17.0/1.10.2/64 intel/17.0/64/17.0.2.174
 
+#remove checkpoints for a benchmark run
 rm /tigress/alexeys/model_checkpoints/*
+rm /tigress/alexeys/results/*
 rm /tigress/alexeys/csv_logs/*
 rm /tigress/alexeys/Graph/*
+rm /tigress/alexeys/normalization/*
+
+export OMPI_MCA_btl="tcp,self,sm"
 
 srun python mpi_learn.py
