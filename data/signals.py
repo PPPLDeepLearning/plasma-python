43c8843d01466fa704b836036fed1d5e4520efc4
Julian Kates-Harbeck <julian@vpn-client-143.pppl.gov>
2017-10-21 19:50:41 -0400

    modified jet and d3d data fetching routines to more robustly handle profile data. Added d3d current error signals, changed d3d plasma current to ipspr15V and jet spatial profiles and jet scalar temperature signal. The jet signals have 50ms sampling times.

--- data/signals.py
+++ data/signals.py
@@ -50,19 +50,7 @@ def fetch_d3d_data(signal_path,shot,c=None):
 		data  = c.get('_s = '+signal).data()
 		data_units = c.get('units_of(_s)').data()  
 		rank = np.ndim(data)	
-		if rank > 1:
-			xdata = c.get('dim_of(_s,1)').data()
-			xunits = get_units('dim_of(_s,1)')
-			ydata 	= c.get('dim_of(_s)').data()
-			yunits = get_units('dim_of(_s)')
-		else:
-			xdata = c.get('dim_of(_s)').data()
-			xunits = get_units('dim_of(_s)')
 		found = True
-		# MDSplus seems to return 2-D arrays transposed.  Change them back.
-		if np.ndim(data) == 2: data = np.transpose(data)
-		if np.ndim(ydata) == 2: ydata = np.transpose(ydata)
-		if np.ndim(xdata) == 2: xdata = np.transpose(xdata)
 
 	except Exception as e:
 		#print(e)
@@ -74,21 +62,35 @@ def fetch_d3d_data(signal_path,shot,c=None):
 		#print("not in full path {}".format(signal))
 		data = c.get('_s = ptdata2("'+signal+'",'+str(shot)+')').data()
 		if len(data) != 1:
-			xdata = c.get('dim_of(_s)').data()
-			rank = 1
+			rank = np.ndim(data)	
 			found = True
 	# Retrieve data from Pseudo-pointname if not in ptdata
 	if not found:
 		#print("not in PTDATA {}".format(signal))
 		data = c.get('_s = pseudo("'+signal+'",'+str(shot)+')').data()
 		if len(data) != 1:
-			xdata = c.get('dim_of(_s)').data()
-			rank = 1
+			rank = np.ndim(data)	
 			found = True
 	#this means the signal wasn't found
 	if not found:  
 		print ("No such signal: {}".format(signal))
 		pass
+
+	# get time base
+	if found:
+		if rank > 1:
+			xdata = c.get('dim_of(_s,1)').data()
+			xunits = get_units('dim_of(_s,1)')
+			ydata 	= c.get('dim_of(_s)').data()
+			yunits = get_units('dim_of(_s)')
+		else:
+			xdata = c.get('dim_of(_s)').data()
+			xunits = get_units('dim_of(_s)')
+
+	# MDSplus seems to return 2-D arrays transposed.  Change them back.
+	if np.ndim(data) == 2: data = np.transpose(data)
+	if np.ndim(ydata) == 2: ydata = np.transpose(ydata)
+	if np.ndim(xdata) == 2: xdata = np.transpose(xdata)
 
     # print '   GADATA Retrieval Time : ',time.time() - t0
 	xdata = xdata*1e-3#time is measued in ms
@@ -96,10 +98,24 @@ def fetch_d3d_data(signal_path,shot,c=None):
 
 
 def fetch_jet_data(signal_path,shot_num,c):
-	data = c.get('_sig=jet("{}/",{})'.format(signal_path,shot_num)).data()
-	time = c.get('_sig=dim_of(jet("{}/",{}))'.format(signal_path,shot_num)).data()
-	found = True
-	return time,data,None,found
+	found = False
+	time = np.array([0])
+	ydata = None
+	data = np.array([0])
+	try:
+		data = c.get('_sig=jet("{}/",{})'.format(signal_path,shot_num)).data()
+		if np.ndim(data) == 2:
+			data = np.transpose(data)
+			time = c.get('_sig=dim_of(jet("{}/",{}),1)'.format(signal_path,shot_num)).data()
+			ydata = c.get('_sig=dim_of(jet("{}/",{}),0)'.format(signal_path,shot_num)).data()
+		else:
+			time = c.get('_sig=dim_of(jet("{}/",{}))'.format(signal_path,shot_num)).data()
+		found = True
+	except Exception as e:
+		#print(e)
+		#sys.stdout.flush()
+		pass
+	return time,data,ydata,found
 
 def fetch_nstx_data(signal_path,shot_num,c):
 	tree,tag = get_tree_and_tag(signal_path)
@@ -119,13 +135,22 @@ nstx = Machine("nstx","skylark.pppl.gov:8501::",fetch_nstx_data,max_cores=8)
 
 all_machines = [d3d,jet]
 
-profile_num_channels = 32
+profile_num_channels = 64
 etemp_profile = ProfileSignal("Electron temperature profile",["ZIPFIT01/PROFILES.ETEMPFIT"],[d3d],causal_shifts=[10],mapping_range=(0,1),num_channels=profile_num_channels)
 edens_profile = ProfileSignal("Electron density profile",["ZIPFIT01/PROFILES.EDENSFIT"],[d3d],causal_shifts=[10],mapping_range=(0,1),num_channels=profile_num_channels)
 
+# epress_profile_spatial = ProfileSignal("Electron pressure profile",["ppf/hrts/pe/"],[jet],causal_shifts=[25],mapping_range=(2,4),num_channels=profile_num_channels)
+etemp_profile_spatial = ProfileSignal("Electron temperature profile",["ppf/hrts/te/"],[jet],causal_shifts=[25],mapping_range=(2,4),num_channels=profile_num_channels)
+edens_profile_spatial = ProfileSignal("Electron density profile",["ppf/hrts/ne/"],[jet],causal_shifts=[25],mapping_range=(2,4),num_channels=profile_num_channels)
+etemp = Signal("electron temperature",["ppf/hrtx/te0/"],[jet],causal_shifts=[25])
+# epress = Signal("electron pressure",["ppf/hrtx/pe0/"],[jet],causal_shifts=[25])
+
 q95 = Signal("q95 safety factor",['ppf/efit/q95',"EFIT01/RESULTS.AEQDSK.Q95"],[jet,d3d],causal_shifts=[15,10],normalize=False)
 
-ip = Signal("plasma current",["jpf/da/c2-ipla","d3d/ipsip"],[jet,d3d],is_ip=True)
+ip = Signal("plasma current",["jpf/da/c2-ipla","d3d/ipspr15V"],[jet,d3d],is_ip=True) #"d3d/ipsip" was used before, ipspr15V seems to be available for a superset of shots.
+iptarget = Signal("plasma current target",["d3d/ipsiptargt"],[d3d])
+iperr = Signal("plasma current error",["d3d/ipeecoil"],[d3d])
+
 li = Signal("internal inductance",["jpf/gs/bl-li<s","d3d/efsli"],[jet,d3d])
 lm = Signal("Locked mode amplitude",['jpf/da/c2-loca','d3d/dusbradial'],[jet,d3d])
 dens = Signal("Plasma density",['jpf/df/g1r-lid:003','d3d/dssdenest'],[jet,d3d])
@@ -149,10 +174,17 @@ tmfreq2 = Signal("Tearing Mode frequency (rotating 3/2)", ['d3d/nssfrqn2l'],[d3d
 ipdirect = Signal("plasma current direction",["d3d/iptdirect"],[d3d])
 
 #for downloading
-all_signals = {'q95':q95,'li':li,'ip':ip,'betan':betan,'energy':energy,'lm':lm,'dens':dens,'pradcore':pradcore,
-'pradedge':pradedge,'pradtot':pradtot,'pin':pin,
-'torquein':torquein,'tmamp1':tmamp1,'tmamp2':tmamp2,'tmfreq1':tmfreq1,'tmfreq2':tmfreq2,
-'pechin':pechin,'energydt':energydt,'ipdirect':ipdirect,'etemp_profile':etemp_profile,'edens_profile':edens_profile}
+all_signals = {'q95':q95,'li':li,'ip':ip,
+'betan':betan,'energy':energy,'lm':lm,'dens':dens,
+'pradcore':pradcore,'pradedge':pradedge,'pradtot':pradtot,
+'pin':pin,'torquein':torquein,
+'tmamp1':tmamp1,'tmamp2':tmamp2,'tmfreq1':tmfreq1,'tmfreq2':tmfreq2,
+'pechin':pechin,'energydt':energydt,
+'ipdirect':ipdirect,'iptarget':iptarget,'iperr':iperr,
+'etemp_profile':etemp_profile,'edens_profile':edens_profile,
+'etemp_profile_spatial':etemp_profile_spatial,'edens_profile_spatial':edens_profile_spatial,
+'etemp':etemp
+}
 
 #for actual data analysis
 #all_signals_restricted = [q95,li,ip,energy,lm,dens,pradcore,pradtot,pin,etemp_profile,edens_profile]
