b47ef7de1394a777c128357a4f51249a2fc749d8
Julian Kates-Harbeck <julian@Julians-MacBook-Pro.local>
2017-10-28 15:01:42 -0400

    spaces to tabs

--- plasma/utils/processing.py
+++ plasma/utils/processing.py
@@ -20,8 +20,8 @@ import sys
 #that is less than or equal than the time point in question
 #and interpolates to there.
 def time_sensitive_interp(x,t,t_new):
-    indices = np.maximum(0,np.searchsorted(t,t_new,side='right')-1)
-    return x[indices]
+	indices = np.maximum(0,np.searchsorted(t,t_new,side='right')-1)
+	return x[indices]
 
 def resample_signal(t,sig,tmin,tmax,dt,precision_str='float32'):
 	order = np.argsort(t)
@@ -31,10 +31,9 @@ def resample_signal(t,sig,tmin,tmax,dt,precision_str='float32'):
 	tt = np.arange(tmin,tmax,dt,dtype=precision_str)
 	sig_interp = np.zeros((len(tt),sig_width),dtype=precision_str)
 	for i in range(sig_width):
-        sig_interp[:,i] = time_sensitive_interp(sig[:,i],t,tt) #make sure to not use future information
+		sig_interp[:,i] = time_sensitive_interp(sig[:,i],t,tt) #make sure to not use future information
 		# f = UnivariateSpline(t,sig[:,i],s=0,k=1,ext=0)
 		# sig_interp[:,i] = f(tt)
-
 	if(np.any(np.isnan(sig_interp))):
 		print("signals contains nan")
 	if(np.any(t[1:] - t[:-1] <= 0)):
@@ -53,53 +52,53 @@ def cut_and_resample_signal(t,sig,tmin,tmax,dt,precision_str):
 	return resample_signal(t,sig,tmin,tmax,dt,precision_str)
 
 def get_individual_shot_file(prepath,shot_num,ext='.txt'):
-    return prepath + str(shot_num) + ext 
+	return prepath + str(shot_num) + ext 
 
 def append_to_filename(path,to_append):
-    ending_idx = path.rfind('.')
-    new_path = path[:ending_idx] + to_append + path[ending_idx:]
-    return new_path
+	ending_idx = path.rfind('.')
+	new_path = path[:ending_idx] + to_append + path[ending_idx:]
+	return new_path
 
 def train_test_split(x,frac,do_shuffle=False):
-    if not isinstance(x,np.ndarray):
-        return train_test_split_robust(x,frac,do_shuffle)
-    mask = np.array(range(len(x))) < frac*len(x)
-    if do_shuffle:
-        np.random.shuffle(mask)
-    return x[mask],x[~mask]
+	if not isinstance(x,np.ndarray):
+		return train_test_split_robust(x,frac,do_shuffle)
+	mask = np.array(range(len(x))) < frac*len(x)
+	if do_shuffle:
+		np.random.shuffle(mask)
+	return x[mask],x[~mask]
 
 def train_test_split_robust(x,frac,do_shuffle=False):
-    mask = np.array(range(len(x))) < frac*len(x)
-    if do_shuffle:
-        np.random.shuffle(mask)
-    train = []
-    test = []
-    for (i,_x) in enumerate(x):
-        if mask[i]:
-            train.append(_x)
-        else:
-            test.append(_x)
-    return train,test
+	mask = np.array(range(len(x))) < frac*len(x)
+	if do_shuffle:
+		np.random.shuffle(mask)
+	train = []
+	test = []
+	for (i,_x) in enumerate(x):
+		if mask[i]:
+			train.append(_x)
+		else:
+			test.append(_x)
+	return train,test
 
 def train_test_split_all(x,frac,do_shuffle=True):
-    groups = []
-    length = len(x[0])
-    mask = np.array(range(length)) < frac*length
-    if do_shuffle:
-        np.random.shuffle(mask)
-    for item in x:
-        groups.append((item[mask],item[~mask]))
-    return groups
+	groups = []
+	length = len(x[0])
+	mask = np.array(range(length)) < frac*length
+	if do_shuffle:
+		np.random.shuffle(mask)
+	for item in x:
+		groups.append((item[mask],item[~mask]))
+	return groups
 
 
 def concatenate_sublists(superlist):
-    return list(itertools.chain.from_iterable(superlist))
+	return list(itertools.chain.from_iterable(superlist))
 
 def get_signal_slices(signals_superlist):
-    indices_superlist = []
-    signals_so_far = 0
-    for sublist in signals_superlist:
-        indices_sublist = signals_so_far + np.array(range(len(sublist)))
-        signals_so_far += len(sublist)
-        indices_superlist.append(indices_sublist)
-    return indices_superlist
+	indices_superlist = []
+	signals_so_far = 0
+	for sublist in signals_superlist:
+		indices_sublist = signals_so_far + np.array(range(len(sublist)))
+		signals_so_far += len(sublist)
+		indices_superlist.append(indices_sublist)
+	return indices_superlist
