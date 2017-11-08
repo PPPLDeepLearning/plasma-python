ab841a473e994018eb3910800b4d7b64fcf03eef
Julian Kates-Harbeck <juliankh91@gmail.com>
2017-11-08 02:13:11 -0500

    updates to get_mdsplus_data

--- data/get_mdsplus_data.py
+++ data/get_mdsplus_data.py
@@ -3,9 +3,9 @@ from data.signals import *
 import plasma.conf
 
 
-prepath = '/cscratch/share/frnn/'#'/p/datad2/'
+prepath = '/p/datad2/' #'/cscratch/share/frnn/'#'/p/datad2/'
 shot_numbers_path = 'shot_lists/'
-save_path = 'signal_data/'
+save_path = 'signal_data_new/'
 machine = d3d#jet#d3d 
 signals = all_signals#jet_signals#d3d_signals
 print('using signals: ')
@@ -13,7 +13,7 @@ print(signals)
 
 # shot_list_files = plasma.conf.jet_full
 #shot_list_files = plasma.conf.d3d_full
-shot_list_files = plasma.conf.d3d_1000
+shot_list_files = conf['paths']['shot_files']#plasma.conf.d3d_100
 
 download_all_shot_numbers(prepath,save_path,shot_list_files,signals)
 
