d3f9f397fdc1306a8db4757facd8e5943eac9b3a
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-11-20 21:06:52 -0500

    Initial set of cunctions for PBS

--- plasma/utils/batch_jobs.py
+++ plasma/utils/batch_jobs.py
@@ -30,11 +30,19 @@ def start_slurm_job(subdir,num_nodes,i,conf,shallow,env_name="frnn",env_type="an
     script = create_slurm_script(subdir,num_nodes,i,executable_name,use_mpi,env_name,env_type)
     sp.Popen("sbatch "+script,shell=True)
 
+
+def start_pbs_job(subdir,num_nodes,i,conf,shallow,env_name="frnn",env_type="anaconda"):
+    executable_name,use_mpi = get_executable_name(conf)
+    os.system(" ".join(["cp -p",executable_name,subdir]))
+    script = create_pbs_script(subdir,num_nodes,i,executable_name,use_mpi,env_name,env_type)
+    sp.Popen("qsub "+script,shell=True)
+
+
 def create_slurm_script(subdir,num_nodes,idx,executable_name,use_mpi,env_name="frnn",env_type="anaconda"):
     filename = "run_{}_nodes.cmd".format(num_nodes)
     filepath = subdir+filename
     user = getpass.getuser()
-    sbatch_header = create_sbatch_header(num_nodes,use_mpi,idx)
+    sbatch_header = create_slurm_header(num_nodes,use_mpi,idx)
     with open(filepath,"w") as f:
         for line in sbatch_header:
             f.write(line)
@@ -49,7 +57,27 @@ def create_slurm_script(subdir,num_nodes,idx,executable_name,use_mpi,env_name="f
 
     return filepath
 
-def create_sbatch_header(num_nodes,use_mpi,idx):
+def create_pbs_script(subdir,num_nodes,idx,executable_name,use_mpi,env_name="frnn",env_type="anaconda"):
+    filename = "run_{}_nodes.cmd".format(num_nodes)
+    filepath = subdir+filename
+    user = getpass.getuser()
+    sbatch_header = create_pbs_header(num_nodes,use_mpi,idx)
+    with open(filepath,"w") as f:
+        for line in sbatch_header:
+            f.write(line)
+        #f.write('export HOME=/lustre/atlas/proj-shared/fus117\n')
+        #f.write('cd $HOME/PPPL/plasma-python/examples\n')
+        f.write('source $MODULESHOME/init/bash\n')
+        f.write('module load tensorflow\n')
+        # f.write('rm $HOME/tigress/alexeys/model_checkpoints/*\n')
+        f.write('cd {}\n'.format(subdir))
+        f.write('aprun -n {} -N1 env PYTHONHASHSEED=0 env KERAS_HOME={} singularity exec $TENSORFLOW_CONTAINER python3 {}\n'.format(str(num_nodes),subdir,executable_name))
+        f.write('echo "done."')
+
+    return filepath
+
+
+def create_slurm_header(num_nodes,use_mpi,idx):
     if not use_mpi:
         assert(num_nodes == 1)
     lines = []
@@ -68,6 +96,20 @@ def create_sbatch_header(num_nodes,use_mpi,idx):
     lines.append('#SBATCH -o {}.out\n'.format(idx))
     lines.append('\n\n')
     return lines
+
+def create_pbs_header(num_nodes,use_mpi,idx):
+    if not use_mpi:
+        assert(num_nodes == 1)
+    lines = []
+    lines.append('#!/bin/bash\n')
+
+    lines.append('#PBS -A FUS117\n')
+    lines.append('#PBS -l walltime=02:00:00\n')
+    lines.append('#PBS -l nodes='+str(num_nodes)+'\n')
+    lines.append('#PBS -o {}.out\n'.format(idx))
+    lines.append('\n\n')
+    return lines
+
 
 def copy_files_to_environment(subdir):
     from plasma.conf import conf
