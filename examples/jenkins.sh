bcf0a1182b77fd4efab9fad817003ebe49a91021
Alexey Svyatkovskiy <ASvyatkovskiy@users.noreply.github.com>
2017-12-17 19:57:42 -0500

    Changes to Jenkins as we move to pull request based trigger

--- examples/jenkins.sh
+++ examples/jenkins.sh
@@ -4,13 +4,26 @@ rm /tigress/alexeys/model_checkpoints/*
 
 ls ${PWD}
 
-module load anaconda
+echo "Jenkins test Python3.6"
+export PYTHONHASHSEED=0
+module load anaconda3
+source activate PPPL_dev3
 module load cudatoolkit/8.0
-module load openmpi/intel-17.0/2.1.0/64 intel/17.0/64/17.0.4.196 intel-mkl/2017.3/4/64
 module load cudnn/cuda-8.0/6.0
-source activate PPPL
-
+module load openmpi/cuda-8.0/intel-17.0/2.1.0/64
+module load intel/17.0/64/17.0.4.196
 export OMPI_MCA_btl="tcp,self,sm"
 
 echo $SLURM_NODELIST
+srun python mpi_learn.py
+
+echo "Jenkins test Python2.7"
+module purge
+module load anaconda
+source activate PPPL
+module load cudatoolkit/8.0
+module load cudnn/cuda-8.0/6.0
+module load openmpi/cuda-8.0/intel-17.0/2.1.0/64
+module load intel/17.0/64/17.0.4.196
+
 srun python mpi_learn.py
