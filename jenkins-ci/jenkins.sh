a79e511fda749d8f34869bac0db07a51e4b3b515
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-12-26 17:40:03 -0500

    Roll back to the single script option

--- jenkins-ci/jenkins.sh
+++ jenkins-ci/jenkins.sh
@@ -21,3 +21,20 @@ sed -i -e 's/num_epochs: 1000/num_epochs: 2/g' conf.yaml
 echo $SLURM_NODELIST
 cd examples
 srun python mpi_learn.py
+
+echo "Jenkins test Python2.7"
+rm /tigress/alexeys/model_checkpoints/*
+rm -rf /tigress/alexeys/normalization
+
+source deactivate
+module purge
+module load anaconda
+source activate PPPL
+module load cudatoolkit/8.0
+module load cudnn/cuda-8.0/6.0
+module load openmpi/cuda-8.0/intel-17.0/2.1.0/64
+module load intel/17.0/64/17.0.4.196
+
+echo $SLURM_NODELIST
+cd examples
+srun python mpi_learn.py
