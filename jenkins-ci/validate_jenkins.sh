a2cd354fd7fbace3b7eb9fa57466ad5c2bf0c40e
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2017-12-29 10:06:18 -0500

    Validate jenkins

---
+++ jenkins-ci/validate_jenkins.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export OMPI_MCA_btl="tcp,self,sm"
+
+module load anaconda3
+source activate PPPL_dev3
+module load cudatoolkit/8.0
+module load cudnn/cuda-8.0/6.0
+module load openmpi/cuda-8.0/intel-17.0/2.1.0/64
+module load intel/17.0/64/17.0.4.196
+
+cd /home/alexeys/jenkins/workspace/FRNM/PPPL
+python setup.py install
+
+echo `which python`
+echo `which mpicc`
+
+echo ${PWD}
+echo $SLURM_NODELIST
+
+cd examples
+echo ${PWD}
+ls ${PWD}
+
+srun python validate_jenkins.py
