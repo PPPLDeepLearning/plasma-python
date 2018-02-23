151738c743d1bb1e404598a25bae323296d531d0
ASvyatkovskiy <asvyatkovskiy@gmail.com>
2018-02-23 15:53:40 -0500

    Use scratch gpfs on Tigergpu - SSDs

--- examples/slurm.cmd
+++ examples/slurm.cmd
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH -t 01:30:00
+#SBATCH -t 01:00:00
 #SBATCH -N 3
 #SBATCH --ntasks-per-node=4
 #SBATCH --ntasks-per-socket=2
@@ -7,21 +7,19 @@
 #SBATCH -c 4
 #SBATCH --mem-per-cpu=0
 
-export PYTHONHASHSEED=0
-module load anaconda
-source activate pppl
+module load anaconda/4.4.0
+source activate PPPL
 module load cudatoolkit/8.0
 module load cudnn/cuda-8.0/6.0
 module load openmpi/cuda-8.0/intel-17.0/2.1.0/64
-module load intel/17.0/64/17.0.4.196 intel-mkl/2017.3/4/64
+module load intel/17.0/64/17.0.4.196
 
 #remove checkpoints for a benchmark run
-rm /tigress/$USER/model_checkpoints/*
-rm /tigress/$USER/results/*
-rm /tigress/$USER/csv_logs/*
-rm /tigress/$USER/Graph/*
-rm /tigress/$USER/normalization/*
+rm /scratch/gpfs/$USER/model_checkpoints/*
+rm /scratch/gpfs/$USER/results/*
+rm /scratch/gpfs/$USER/csv_logs/*
+rm /scratch/gpfs/$USER/Graph/*
+rm /scratch/gpfs/$USER/normalization/*
 
 export OMPI_MCA_btl="tcp,self,sm"
-
 srun python mpi_learn.py
