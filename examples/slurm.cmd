5dd58c81837d46bacf65bc76d60193eec47253f6
Kyle Gerard Felker <kfelker@princeton.edu>
2019-10-16 14:27:43 -0400

    Update module versions, etc. in slurm.cmd
    Make the script consistent with
    https://github.com/PPPLDeepLearning/plasma-python/blob/master/docs/PrincetonUTutorial.md

--- examples/slurm.cmd
+++ examples/slurm.cmd
@@ -1,25 +1,29 @@
 #!/bin/bash
 #SBATCH -t 01:00:00
-#SBATCH -N 3
+#SBATCH -N 4
 #SBATCH --ntasks-per-node=4
 #SBATCH --ntasks-per-socket=2
 #SBATCH --gres=gpu:4
 #SBATCH -c 4
 #SBATCH --mem-per-cpu=0
 
-module load anaconda/4.4.0
-source activate PPPL
-module load cudatoolkit/8.0
-module load cudnn/cuda-8.0/6.0
-module load openmpi/cuda-8.0/intel-17.0/2.1.0/64
-module load intel/17.0/64/17.0.4.196
+# Example Slurm configuration for TigerGPU nodes (4 nodes, 16 GPUs total)
+# Each node = 2.4 GHz Xeon Broadwell E5-2680 v4 + 4x 1328 MHz P100 GPU
 
-#remove checkpoints for a benchmark run
-rm /scratch/gpfs/$USER/model_checkpoints/*
-rm /scratch/gpfs/$USER/results/*
-rm /scratch/gpfs/$USER/csv_logs/*
-rm /scratch/gpfs/$USER/Graph/*
-rm /scratch/gpfs/$USER/normalization/*
+module load anaconda3
+conda activate my_env
+module load cudatoolkit
+module load cudnn
+module load openmpi/cuda-8.0/intel-17.0/3.0.0/64
+module load intel/19.0/64/19.0.3.199
+module load hdf5/intel-17.0/intel-mpi/1.10.0
 
-export OMPI_MCA_btl="tcp,self,sm"
+# remove checkpoints for a benchmark run
+rm /tigress/$USER/model_checkpoints/*
+rm /tigress/$USER/results/*
+rm /tigress/$USER/csv_logs/*
+rm /tigress/$USER/Graph/*
+rm /tigress/$USER/normalization/*
+
+export OMPI_MCA_btl="tcp,self,vader"
 srun python mpi_learn.py
