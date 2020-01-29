PPPLDeepLearning/plasma-python

commit 14d4a35df38b84c1b693e1d37cfe9c3afc19892b

Remove duplicate d3d/ subdir for 0D signals in hierarchy of signal_data/

data/d3d_signals.py
@@ -32,8 +32,6 @@ signal_paths = [
     'bmstinj', ]  # Total Beam Torque
 # 'pcechpwrf'] #Total ECH Power Not always on!
 
-signal_paths = ['d3d/' + path for path in signal_paths]
-
 # 0D EFIT signals
 signal_paths += ['EFIT01/RESULTS.AEQDSK.Q95']
 

plasma/models/targets.py
@@ -15,6 +15,11 @@ if g.comm is not None:
     g.flush_all_inorder()
     g.comm.Barrier()
 
+# TODO(KGF): remove unused threshold_range() methods (#54)
+
+# remapper() method, used only in normalize.py, implicitly knows the
+# transformation applied to Shot.ttd within Shot.convert_to_ttd()
+
 
 # Requirement: larger value must mean disruption more likely.
 class Target(object):
@@ -28,6 +33,7 @@ class Target(object):
 
     @abc.abstractmethod
     def remapper(ttd, T_warning):
+        # TODO(KGF): base class directly uses ttd=log(TTD+dt/10) quantity
         return -ttd
 
     @abc.abstractmethod
@@ -47,6 +53,7 @@ class BinaryTarget(Target):
 
     @staticmethod
     def remapper(ttd, T_warning, as_array_of_shots=True):
+        # TODO(KGF): see below comment in HingeTarget.remapper()
         binary_ttd = 0*ttd
         mask = ttd < np.log10(T_warning)
         binary_ttd[mask] = 1.0
@@ -88,11 +95,11 @@ class TTDInvTarget(Target):
 
     @staticmethod
     def remapper(ttd, T_warning):
-        eps = 1e-4
-        ttd = 10**(ttd)
+        eps = 1e-4  # hardcoded "avoid division by zero"
+        ttd = 10**(ttd)  # see below comment about undoing log transformation
         mask = ttd < T_warning
         ttd[~mask] = T_warning
-        ttd = (1.0)/(ttd+eps)  # T_warning
+        ttd = (1.0)/(ttd + eps)
         return ttd
 
     @staticmethod
@@ -111,6 +118,8 @@ class TTDLinearTarget(Target):
 
     @staticmethod
     def remapper(ttd, T_warning):
+        # TODO(KGF): this next line "undoes" the log-transform in shots.py
+        # Shot.convert_to_ttd() (except for small offset of +dt/10)
         ttd = 10**(ttd)
         mask = ttd < T_warning
         ttd[~mask] = 0  # T_warning
@@ -184,6 +193,7 @@ class MaxHingeTarget(Target):
 
     @staticmethod
     def remapper(ttd, T_warning, as_array_of_shots=True):
+        # TODO(KGF): see below comment in HingeTarget.remapper()
         binary_ttd = 0*ttd
         mask = ttd < np.log10(T_warning)
         binary_ttd[mask] = 1.0
@@ -209,6 +219,7 @@ class HingeTarget(Target):
 
     @staticmethod
     def remapper(ttd, T_warning, as_array_of_shots=True):
+        # TODO(KGF): zeros the ttd=log(TTD+dt/10) (just reuses shape)
         binary_ttd = 0*ttd
         mask = ttd < np.log10(T_warning)
         binary_ttd[mask] = 1.0

commit f3951ef6438126d93e19f1a6b504266ff2014763

Fix bug introduced by 14d4a35; actually remove 'd3d/' from sig paths
Bug caused all shots to be omitted during preprocessing due to missing
0D signal paths

Remove unused data/jet_signals.py, data/d3d_signals.py. Haven't really
been touched since April 2017 and appear to be dead code since @jnkh
introduced the Signals() class and signals.py

data/d3d_signals.py
@@ -1,99 +1,0 @@
-# D3D signal hierarchy
-# ------------------------------------------------------------------------
-# User only needs to look at 1st and last sections
-#    - conf.py only needs to import signals_dirs and signals_masks
-#    - get_mdsplus_data.py only needs signals_dirs and download_masks
-#    - performance_analysis_utils.py needs :
-#         - signals_dirs, plot_masks, ppf_labels, jpf_labels
-# ------------------------------------------------------------------------
-################
-# Signal names #
-################
-# This section contains all the exact D3D signal strings and their
-# groupings by type and dimensionality.
-# User should not touch this. Use for reference
-
-
-# 0D signals #
-signal_paths = [
-    'efsli',  # Internal Inductance
-    'ipsip',  # Plasma Current
-    'efsbetan',  # Normalized Beta
-    'efswmhd',  # Stored Energy
-    'nssampn1l',  # Tearing Mode Amplitude (rotating 2/1)
-    'nssfrqn1l',  # Tearing Mode Frequency (rotating 2/1)
-    'nssampn2l',  # Tearing Mode Amplitude (rotating 3/2)
-    'nssfrqn2l',  # Tearing Mode Frequency (rotating 3/2)
-    'dusbradial',  # LM Amplitude
-    'dssdenest',  # Plasma Density
-    r'\bol_l15_p',  # Radiated Power core
-    r'\bol_l03_p',  # Radiated Power Edge
-    'bmspinj',  # Total Beam Power
-    'bmstinj', ]  # Total Beam Torque
-# 'pcechpwrf'] #Total ECH Power Not always on!
-
-# 0D EFIT signals
-signal_paths += ['EFIT01/RESULTS.AEQDSK.Q95']
-
-# 1D EFIT signals
-# signal_paths += [
-# 'AOT/EQU.t_e', # electron temperature profile vs rho
-# 'AOT/EQU.dens_e'] # electron density profile vs rho
-
-# these signals seem to give more reliable data
-signal_paths += [
-    'ZIPFIT01/PROFILES.ETEMPFIT',  # electron temperature profile vs rho
-    'ZIPFIT01/PROFILES.EDENSFIT']  # electron density profile vs rho
-
-# make into list of lists format to be consistent with jet_signals.py
-signal_paths = [[path] for path in signal_paths]
-
-# format : 'tree/signal_path' for each path
-signals_dirs = signal_paths
-
-
-##################################################
-#             USER SELECTIONS                    #
-##################################################
-
-
-##################################
-# Select signals for downloading #
-##################################
-
-# Default pass to get_mdsplus_data.py: download all above signals
-download_masks = [[True]*len(sig_list) for sig_list in signals_dirs]
-# download_masks[-1] = [False] # enable/disable temperature profile
-# download_masks[-2] = [False] # enable/disable density profile
-
-#######################################
-# Select signals for training/testing #
-#######################################
-
-# Default pass to conf.py: train with all above signals
-signals_masks = [[True]*len(sig_list) for sig_list in signals_dirs]
-signals_masks[-1] = [False]  # enable/disable temperature profile
-signals_masks[-2] = [False]  # enable/disable density profile
-
-# num_signals = sum([group.count(True) for i, group in
-# enumerate(jet_signals.signals_masks)]
-
-###########################################
-# Select signals for performance analysis #
-###########################################
-
-# User selects these by signal name
-plot_masks = [[True]*len(sig_list) for sig_list in signals_dirs]
-
-# LaTeX strings for performance analysis, sorted in lists by signal_group
-group_labels = [[r' $I_{plasma}$ [A]'],
-                [r' Mode L. A. [A]'],
-                [r' $P_{radiated}$ [W]'],  # 0d radiation, db/
-                [r' $P_{radiated}$ [W]'],  # 1d radiation, db/
-                [r' $\rho_{plasma}$ [m^-2]'],
-                [r' $L_{plasma,internal}$'],
-                [r'$\frac{d}{dt} E_{D}$ [W]'],
-                [r' $P_{input}$ [W]'],
-                [r'$E_{D}$'],
-                # ppf signal labels
-                [r'ECE unit?']]

data/jet_signals.py
@@ -1,219 +1,0 @@
-# JET signal hierarchy
-# ------------------------------------------------------------------------
-# User only needs to look at 1st and last sections
-#    - conf.py only needs to import signals_dirs and signals_masks
-#    - get_mdsplus_data.py only needs signals_dirs and download_masks
-#    - performance_analysis_utils.py needs :
-#         - signals_dirs, plot_masks, ppf_labels, jpf_labels
-# ------------------------------------------------------------------------
-################
-# Signal names #
-################
-# This section contains all the exact JET signal strings and their
-# groupings by type and dimensionality.
-# User should not touch this. Use for reference
-
-#############################
-#     0D current signals    #
-#############################
-da_current = ['c2-ipla']  # Plasma Current [A]
-da_lock = ['c2-loca']  # Mode Lock Amplitude [A]
-
-############################
-#     Radiation signals    #
-############################
-# 0D
-db_out = ['b5r-ptot>out']  # Radiated Power [W]
-# 1D vertical signals, don't use signal 16 and 23
-db = []
-db += ['b5vr-pbol:{:03d}'.format(i)
-       for i in range(1, 28) if (i != 16 and i != 23)]
-# 1D horizontal signals
-db += ['b5hr-pbol:{:03d}'.format(i) for i in range(1, 24)]
-
-####################################
-#     1D density signals [m^-2]    #
-####################################
-df = []
-# 4 vertical channels and 4 horizontal channels, dont use signal 1
-df += ['g1r-lid:{:03d}'.format(i) for i in range(2, 9)]
-
-###########################
-#     0D signals et al    #
-###########################
-gs_inductance = ['bl-li<s']  # Plasma Internal Inductance
-gs_fdwdt = ['bl-fdwdt<s']  # Stored Diamagnetic Energy (time derivative)
-gs_power_in = ['bl-ptot<s']  # Total input power [W]
-gs_wmhd = ['bl-wmhd<s']  # Stored Diamagnetic Energy (total)
-gs_gwdens = ['bl-gwdens<s']  # Greenwald density
-gs_torb0 = ['bl-torb0<s']  # Toroidal field on axis
-gs_minrad = ['bl-minrad<s']  # Minor radius
-
-###################################
-#     ECE temperature profiles    #
-###################################
-kk3 = []
-# Temperature of channel i vs time
-kk3 += ['te{:02d}'.format(i) for i in range(1, 97)]
-# Radial position of channel i vs time
-kk3 += ['ra{:02d}'.format(i) for i in range(1, 97)]
-# Radial position of channel i mapped onto midplane vs time
-kk3 += ['rc{:02d}'.format(i) for i in range(1, 97)]
-# General information (PPF)
-kk3 += ['gen']
-
-###################################
-# Signal subsystems and groupings #
-###################################
-# Signal groupings by type/dimensionality
-jpf = [da_current,
-       da_lock,
-       db_out,
-       db,
-       df,
-       gs_inductance,
-       gs_fdwdt,
-       gs_power_in,
-       gs_wmhd,
-       gs_gwdens,
-       gs_torb0,
-       gs_minrad]
-
-ppf = [kk3]
-
-# Manually write strings of actual JET subsystems
-jpf_str = ['da',  # magnetic diagnostic, essential subs
-           'da',
-           'db',  # diagnostic subsys, general pulse radiation
-           'db',
-           'df',  # density diagnostic, essential subsys
-           'gs',  # general services, essential subsys
-           'gs',
-           'gs',
-           'gs',
-           'gs',
-           'gs',
-           'gs']
-
-ppf_str = ['kk3']
-subsys_str = [jpf_str, ppf_str]
-
-#################
-# Signal types  #
-#################
-signal_type = [jpf, ppf]
-signal_type_str = ['jpf', 'ppf']
-
-#########################################
-# Build the full paths from the strings #
-#########################################
-
-signals_dirs = []
-for i in range(0, len(signal_type)):  # jpf or ppf
-    for j in range(0, len(signal_type[i])):  # subsystem/grouping
-        signal_group = []
-        for k in range(0, len(signal_type[i][j])):  # signal name
-            signal_group += [signal_type_str[i] + "/"
-                             + subsys_str[i][j] + "/" + signal_type[i][j][k]]
-        signals_dirs += [signal_group]
-# Print out 2D hierarchy of the signals_dirs
-# for i, group in enumerate(signals_dirs):
-#     for j, signal in enumerate(group):
-#         print(i,j,signal)
-
-##################################################
-#             USER SELECTIONS                    #
-##################################################
-
-
-##################################
-# Select signals for downloading #
-##################################
-
-# Default pass to get_mdsplus_data.py: download all above signals
-download_masks = [[True]*len(sig_list) for sig_list in signals_dirs]
-# download all radiation signals
-download_masks[3] = [False]*len(signals_dirs[3])
-# enable/disable ppf/kk3/gen (only one column)
-download_masks[-1][-1] = [False]
-
-#######################################
-# Select signals for training/testing #
-#######################################
-
-# Default pass to conf.py: train with all above signals, minus gs_fdwdt
-# signals_masks = [[True]*len(sig_list) for sig_list in signals_dirs]
-signals_masks = [[False]*len(sig_list) for sig_list in signals_dirs]
-# jpf = [da_current,
-#        da_lock,
-#        db_out,
-#        db,
-#        df,
-#        gs_inductance,
-#        gs_fdwdt,
-#        gs_power_in,
-#        gs_wmhd,
-#        gs_gwdens,
-#        gs_torb0,
-#        gs_minrad]
-
-
-# signals_masks[8] = [True] # total diamagnetic energy
-# signals_masks[7] = [False] # total input power
-signals_masks[6] = [False]  # time derivative of diamagnetic energy
-# signals_masks[5] = [True] # inductance
-# signals_masks[4] = [False]*len(signals_dirs[4]) #density
-# signals_masks[4][1] = True #central denisty #this was automatically excluded!
-# signals_masks[3] = [True]*len(signals_dirs[3]) #radiation, vertical and
-# horizontal channels
-signals_masks[2] = [False]  # wout
-signals_masks[1] = [False]  # locked mode
-# need to turn the current on for thresholding signal start/end
-signals_masks[0] = [True]
-
-# One way for user to disable a signal_group: know the exact index
-# signals_masks[6] = [False] # Disable time-derivative of stored diamagnetic
-#                              energy
-# signals_masks[3] = [False]*len(signals_dirs[3]) #Disable all radiation
-# signals
-
-# Another way for the user to disable individual signals within a group:
-# Disable 'radial position of channel i vs time' of ECE for all 96 channels
-for i, group in enumerate(signals_dirs):
-    for j, signal in enumerate(group):
-        if 'ra' in signal:
-            signals_masks[i][j] = False
-
-
-# num_signals = sum([group.count(True)
-#  for i, group in enumerate(jet_signals.signals_masks)]
-###########################################
-# Select signals for performance analysis #
-###########################################
-
-# User selects these by signal name
-plot_masks = [[False]*len(sig_list) for sig_list in signals_dirs]
-# Default: 8 golden 0D signals, and all density channels
-plot_masks[0][0] = True
-
-# plot_masks[1][0] = True
-# plot_masks[2][0] = True
-# plot_masks[4][1] = True
-# plot_masks[4] = [True]*len(signals_dirs[4]) #density
-# plot_masks[5][0] = True
-plot_masks[6][0] = True
-# plot_masks[7][0] = True
-# plot_masks[8][0] = True
-
-# LaTeX strings for performance analysis, sorted in lists by signal_group
-group_labels = [[r' $I_{plasma}$ [A]'],
-                [r' Mode L. A. [A]'],
-                [r' $P_{radiated}$ [W]'],  # 0d radiation, db/
-                [r' $P_{radiated}$ [W]'],  # 1d radiation, db/
-                [r' $\rho_{plasma}$ [m^-2]'],
-                [r' $L_{plasma,internal}$'],
-                [r'$\frac{d}{dt} E_{D}$ [W]'],
-                [r' $P_{input}$ [W]'],
-                [r'$E_{D}$'],
-                # ppf signal labels
-                [r'ECE unit?']]

data/signals.py
@@ -299,90 +299,90 @@ q95t = Signal(
 
 # "d3d/ipsip" was used before, ipspr15V seems to be available for a
 # superset of shots.
-ip = Signal("plasma current", ["jpf/da/c2-ipla", "d3d/ipspr15V"],
+ip = Signal("plasma current", ["jpf/da/c2-ipla", "ipspr15V"],
             [jet, d3d], is_ip=True)
 
-ipt = Signal("plasma current tol", ["jpf/da/c2-ipla", "d3d/ipspr15V"],
+ipt = Signal("plasma current tol", ["jpf/da/c2-ipla", "ipspr15V"],
              [jet, d3d], is_ip=True, data_avail_tolerances=[0.029, 0.029])
-iptarget = Signal("plasma current target", ["d3d/ipsiptargt"], [d3d])
-iptargett = Signal("plasma current target tol", ["d3d/ipsiptargt"], [d3d],
+iptarget = Signal("plasma current target", ["ipsiptargt"], [d3d])
+iptargett = Signal("plasma current target tol", ["ipsiptargt"], [d3d],
                    data_avail_tolerances=[0.029])
-iperr = Signal("plasma current error", ["d3d/ipeecoil"], [d3d])
-iperrt = Signal("plasma current error tol", ["d3d/ipeecoil"], [d3d],
+iperr = Signal("plasma current error", ["ipeecoil"], [d3d])
+iperrt = Signal("plasma current error tol", ["ipeecoil"], [d3d],
                 data_avail_tolerances=[0.029])
 
-li = Signal("internal inductance", ["jpf/gs/bl-li<s", "d3d/efsli"], [jet, d3d])
-lit = Signal("internal inductance tol", ["jpf/gs/bl-li<s", "d3d/efsli"],
+li = Signal("internal inductance", ["jpf/gs/bl-li<s", "efsli"], [jet, d3d])
+lit = Signal("internal inductance tol", ["jpf/gs/bl-li<s", "efsli"],
              [jet, d3d], data_avail_tolerances=[0.029, 0.029])
-lm = Signal("Locked mode amplitude", ['jpf/da/c2-loca', 'd3d/dusbradial'],
+lm = Signal("Locked mode amplitude", ['jpf/da/c2-loca', 'dusbradial'],
             [jet, d3d])
-lmt = Signal("Locked mode amplitude tol", ['jpf/da/c2-loca', 'd3d/dusbradial'],
+lmt = Signal("Locked mode amplitude tol", ['jpf/da/c2-loca', 'dusbradial'],
              [jet, d3d], data_avail_tolerances=[0.029, 0.029])
-dens = Signal("Plasma density", ['jpf/df/g1r-lid:003', 'd3d/dssdenest'],
+dens = Signal("Plasma density", ['jpf/df/g1r-lid:003', 'dssdenest'],
               [jet, d3d], is_strictly_positive=True)
-denst = Signal("Plasma density tol", ['jpf/df/g1r-lid:003', 'd3d/dssdenest'],
+denst = Signal("Plasma density tol", ['jpf/df/g1r-lid:003', 'dssdenest'],
                [jet, d3d], is_strictly_positive=True,
                data_avail_tolerances=[0.029, 0.029])
-energy = Signal("stored energy", ['jpf/gs/bl-wmhd<s', 'd3d/efswmhd'],
+energy = Signal("stored energy", ['jpf/gs/bl-wmhd<s', 'efswmhd'],
                 [jet, d3d])
-energyt = Signal("stored energy tol", ['jpf/gs/bl-wmhd<s', 'd3d/efswmhd'],
+energyt = Signal("stored energy tol", ['jpf/gs/bl-wmhd<s', 'efswmhd'],
                  [jet, d3d], data_avail_tolerances=[0.029, 0.029])
 # Total beam input power
-pin = Signal("Input Power (beam for d3d)", ['jpf/gs/bl-ptot<s', 'd3d/bmspinj'],
+pin = Signal("Input Power (beam for d3d)", ['jpf/gs/bl-ptot<s', 'bmspinj'],
              [jet, d3d])
 pint = Signal("Input Power (beam for d3d) tol",
-              ['jpf/gs/bl-ptot<s', 'd3d/bmspinj'],
+              ['jpf/gs/bl-ptot<s', 'bmspinj'],
               [jet, d3d], data_avail_tolerances=[0.029, 0.029])
 
 pradtot = Signal("Radiated Power", ['jpf/db/b5r-ptot>out'], [jet])
 pradtott = Signal("Radiated Power tol", ['jpf/db/b5r-ptot>out'], [jet],
                   data_avail_tolerances=[0.029])
 # pradtot = Signal("Radiated Power", ['jpf/db/b5r-ptot>out',
-# 'd3d/'+r'\prad_tot'], [jet,d3d])
-# pradcore = ChannelSignal("Radiated Power Core", [ 'd3d/' + r'\bol_l15_p']
+# r'\prad_tot'], [jet,d3d])
+# pradcore = ChannelSignal("Radiated Power Core", [r'\bol_l15_p']
 # ,[d3d])
-# pradedge = ChannelSignal("Radiated Power Edge", ['d3d/' + r'\bol_l03_p'],
+# pradedge = ChannelSignal("Radiated Power Edge", [r'\bol_l03_p'],
 # [d3d])
 pradcore = ChannelSignal("Radiated Power Core",
-                         ['ppf/bolo/kb5h/channel14', 'd3d/' + r'\bol_l15_p'],
+                         ['ppf/bolo/kb5h/channel14', r'\bol_l15_p'],
                          [jet, d3d])
 pradedge = ChannelSignal("Radiated Power Edge",
-                         ['ppf/bolo/kb5h/channel10', 'd3d/' + r'\bol_l03_p'],
+                         ['ppf/bolo/kb5h/channel10', r'\bol_l03_p'],
                          [jet, d3d])
 pradcoret = ChannelSignal("Radiated Power Core tol",
-                          ['ppf/bolo/kb5h/channel14', 'd3d/' + r'\bol_l15_p'],
+                          ['ppf/bolo/kb5h/channel14', r'\bol_l15_p'],
                           [jet, d3d], data_avail_tolerances=[0.029, 0.029])
 pradedget = ChannelSignal("Radiated Power Edge tol",
-                          ['ppf/bolo/kb5h/channel10', 'd3d/' + r'\bol_l03_p'],
+                          ['ppf/bolo/kb5h/channel10', r'\bol_l03_p'],
                           [jet, d3d], data_avail_tolerances=[0.029, 0.029])
-# pechin = Signal("ECH input power, not always on", ['d3d/pcechpwrf'], [d3d])
+# pechin = Signal("ECH input power, not always on", ['pcechpwrf'], [d3d])
 pechin = Signal("ECH input power, not always on",
                 ['RF/ECH.TOTAL.ECHPWRC'], [d3d])
 pechint = Signal("ECH input power, not always on tol",
                  ['RF/ECH.TOTAL.ECHPWRC'], [d3d],
                  data_avail_tolerances=[0.029])
 
-# betan = Signal("Normalized Beta", ['jpf/gs/bl-bndia<s', 'd3d/efsbetan'],
+# betan = Signal("Normalized Beta", ['jpf/gs/bl-bndia<s', 'efsbetan'],
 # [jet, d3d])
-betan = Signal("Normalized Beta", ['d3d/efsbetan'], [d3d])
-betant = Signal("Normalized Beta tol", ['d3d/efsbetan'], [d3d],
+betan = Signal("Normalized Beta", ['efsbetan'], [d3d])
+betant = Signal("Normalized Beta tol", ['efsbetan'], [d3d],
                 data_avail_tolerances=[0.029])
 energydt = Signal(
     "stored energy time derivative", ['jpf/gs/bl-fdwdt<s'], [jet])
 
-torquein = Signal("Input Beam Torque", ['d3d/bmstinj'], [d3d])
-torqueint = Signal("Input Beam Torque tol", ['d3d/bmstinj'], [d3d],
+torquein = Signal("Input Beam Torque", ['bmstinj'], [d3d])
+torqueint = Signal("Input Beam Torque tol", ['bmstinj'], [d3d],
                    data_avail_tolerances=[0.029])
-tmamp1 = Signal("Tearing Mode amplitude (rotating 2/1)", ['d3d/nssampn1l'],
+tmamp1 = Signal("Tearing Mode amplitude (rotating 2/1)", ['nssampn1l'],
                 [d3d])
-tmamp2 = Signal("Tearing Mode amplitude (rotating 3/2)", ['d3d/nssampn2l'],
+tmamp2 = Signal("Tearing Mode amplitude (rotating 3/2)", ['nssampn2l'],
                 [d3d])
-tmfreq1 = Signal("Tearing Mode frequency (rotating 2/1)", ['d3d/nssfrqn1l'],
+tmfreq1 = Signal("Tearing Mode frequency (rotating 2/1)", ['nssfrqn1l'],
                  [d3d])
-tmfreq2 = Signal("Tearing Mode frequency (rotating 3/2)", ['d3d/nssfrqn2l'],
+tmfreq2 = Signal("Tearing Mode frequency (rotating 3/2)", ['nssfrqn2l'],
                  [d3d])
-ipdirect = Signal("plasma current direction", ["d3d/iptdirect"], [d3d])
-ipdirectt = Signal("plasma current direction tol", ["d3d/iptdirect"], [d3d],
+ipdirect = Signal("plasma current direction", ["iptdirect"], [d3d])
+ipdirectt = Signal("plasma current direction tol", ["iptdirect"], [d3d],
                    data_avail_tolerances=[0.029])
 
 # for downloading, modify this to preprocess shots with only a subset of

plasma/preprocessor/preprocess.py
@@ -138,6 +138,7 @@ class Preprocessor(object):
         return shot
 
     def get_individual_channel_dirs(self):
+        # TODO(KGF): unused
         return self.conf['paths']['signals_dirs']
 
     def get_shot_list_path(self):