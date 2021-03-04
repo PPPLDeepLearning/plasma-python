6f92d00331e6c3eb501ba8ed3810e67ff2db2a08
Kyle Gerard Felker <felker@anl.gov>
2021-03-04 15:59:56 -0500

    Update Traverse modules due to RHEL 8 transition around Sep 2020

--- envs/traverse.cmd
+++ envs/traverse.cmd
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-module load anaconda3  # KGF: issue with my shell--- makes conda CLI return nothing
-# must activate conda env before module loads to ensure MPI, etc modules have
-# precedence for setting the environment variables, libraries, etc.
+module load anaconda3
 conda activate frnn
-export OMPI_MCA_btl="tcp,self,vader"
-# module load anaconda3 ------ KGF: DO NOT DO THIS--- reloads base
-# module purge
-module load cudatoolkit
+
+module load cudatoolkit/11.0
 module load cudnn/cuda-10.1/7.6.1
-module load openmpi/gcc/3.1.4/64
-module load hdf5/gcc/openmpi-3.1.4/1.10.5
+
+# after RHEL 8 upgrade
+module load openmpi/gcc/4.0.4/64
+module load hdf5/gcc/openmpi-4.0.4/1.10.6
