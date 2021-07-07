fc02d4d440ed109739e1a2ab5fee86652c465f55
Kyle Gerard Felker <felker@anl.gov>
2021-07-06 20:51:06 -0400

    Bump modules and delete some conda/pip deps on Traverse
    Downgrade h5py for Keras model loading in TF 2.1.x
    https://github.com/tensorflow/tensorflow/issues/44467

--- envs/traverse.cmd
+++ envs/traverse.cmd
@@ -3,8 +3,8 @@
 module load anaconda3
 conda activate frnn
 
-module load cudatoolkit/11.0
-module load cudnn/cuda-10.1/7.6.1
+module load cudatoolkit/11.3
+module load cudnn/cuda-11.x/8.2.0
 
 # after RHEL 8 upgrade
 module load openmpi/gcc/4.0.4/64
